645d49c07bb13491bcc96f8d335def731f55480b
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-01 21:47:16 +0100

    :sparkles: models

---
+++ src/models/Channel.ts
@@ -0,0 +1,19 @@
+export interface Channel {
+	id: bigint;
+	guild_id: bigint;
+	last_message_id: string;
+	last_pin_timestamp: string;
+	name: string;
+	nsfw: boolean;
+	parent_id: bigint;
+	position: number;
+	rate_limit_per_user: number;
+	topic: string | null;
+	type: number;
+	permission_overwrites: {
+		allow: bigint;
+		deny: bigint;
+		id: bigint;
+		type: number;
+	}[];
+}
