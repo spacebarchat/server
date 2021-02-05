d4a89cae8adc57157997feef1ddc0f3b54da5ab1
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-05 15:19:32 +0100

    Config

---
+++ src/Config.ts
@@ -0,0 +1,30 @@
+import "missing-native-js-functions";
+import db from "./Database";
+import { ProviderCache } from "lambert-db";
+var Config: ProviderCache;
+
+export default {
+	init: async function init(opts: DefaultOptions = DefaultOptions) {
+		Config = db.data.config({}).cache();
+		await Config.init();
+		await Config.set(opts.merge(Config.cache || {}));
+	},
+	getAll: function get() {
+		return <DefaultOptions>Config.get();
+	},
+	setAll: function set(val: any) {
+		return Config.set(val);
+	},
+};
+
+export interface DefaultOptions {
+	api: any;
+	gateway: any;
+	voice: any;
+}
+
+export const DefaultOptions: DefaultOptions = {
+	api: {},
+	gateway: {},
+	voice: {},
+};
---
+++ src/Constants.ts
@@ -0,0 +1,3 @@
+import { VerifyOptions } from "jsonwebtoken";
+
+export const JWTOptions: VerifyOptions = { algorithms: ["HS256"] };
