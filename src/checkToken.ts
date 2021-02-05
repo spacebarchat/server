9b3209efd5d421bdb7ede4780b66162ecff41fbd
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-05 15:19:24 +0100

    checkToken

---
+++ src/checkToken.ts
@@ -0,0 +1,13 @@
+import { JWTOptions } from "./Constants";
+import jwt from "jsonwebtoken";
+import Config from "./Config";
+
+export function checkToken(token: string) {
+	return new Promise((res, rej) => {
+		jwt.verify(token, Config.getAll().api.security.jwtSecret, JWTOptions, (err, decoded: any) => {
+			if (err || !decoded) return rej("Invalid Token");
+
+			return res(decoded);
+		});
+	});
+}
