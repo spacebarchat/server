641f817fdeac164ad3c840e67d11ba1707c72438
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-05 15:33:42 +0100

    :art: export functions

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { checkToken } from "./checkToken";
+import Config, { DefaultOptions } from "./Config";
+import Database from "./Database";
+import * as Constants from "./Constants";
+
+export { checkToken, Config, Constants, Database, DefaultOptions };
