f802c37559857b0036bc710b2808bb69ed528c28
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-06 10:09:17 +0100

    moved utils to server-util

--- src/util/String.ts
+++ src/util/String.ts
@@ -1,12 +1,5 @@
 import { Request } from "express";
-import { FieldError, FieldErrors } from "./instanceOf";
-
-export const DOUBLE_WHITE_SPACE = /\s\s+/g;
-export const SPECIAL_CHAR = /[@#`:\r\n\t\f\v\p{C}]/gu;
-
-export function trimSpecial(str: string) {
-	return str.replace(SPECIAL_CHAR, "").replace(DOUBLE_WHITE_SPACE, " ").trim();
-}
+import { FieldErrors } from "./instanceOf";
 
 export function checkLength(str: string, min: number, max: number, key: string, req: Request) {
 	if (str.length < min || str.length > max) {
