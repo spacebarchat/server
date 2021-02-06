71ace90f070ac5fd23872f5b2a4ce42b4bed998d
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-06 17:08:33 +0100

    :bug: fix identify schema

--- src/Schema/Identify.ts
+++ src/Schema/Identify.ts
@@ -2,13 +2,13 @@ import { ActivitySchema } from "./Activity";
 
 export const IdentifySchema = {
 	token: String,
-	properties: {
+	intents: BigInt, // discord uses a Integer for bitfields we use bigints tho. | instanceOf will automatically convert the Number to a BigInt
+	$properties: {
 		// bruh discord really uses $ in the property key, so we need to double prefix it, because instanceOf treats $ (prefix) as a optional key
 		$$os: String,
 		$$browser: String,
 		$$device: String,
 	},
-	intents: BigInt, // discord uses a Integer for bitfields we use bigints tho. | instanceOf will automatically convert the Number to a BigInt
 	$presence: ActivitySchema,
 	$compress: Boolean,
 	$large_threshold: Number,
