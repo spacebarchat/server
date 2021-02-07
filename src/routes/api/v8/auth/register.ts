fb7c119bf1f87341be8d3bd9aaf154c7192fcd58
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-07 11:25:39 +0100

    :bug: prevent @googlemail.com trick

--- src/routes/api/v8/auth/register.ts
+++ src/routes/api/v8/auth/register.ts
@@ -231,7 +231,8 @@ export function adjustEmail(email: string) {
 	const domain = parts[5];
 	const user = parts[1];
 
-	if (domain === "gmail.com") {
+	// TODO: check accounts with uncommon email domains
+	if (domain === "gmail.com" || domain === "googlemail.com") {
 		// replace .dots and +alternatives -> Gmail Dot Trick https://support.google.com/mail/answer/7436150 and https://generator.email/blog/gmail-generator
 		return user.replace(/[.]|(\+.*)/g, "") + "@gmail.com";
 	}
