6030b861219b9f9a63a490b6dfa2da25791e70ef
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-16 13:30:54 +0100

    :sparkles: load .env file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ process.on("unhandledRejection", console.error);
 setTimeout(() => {}, 100000000);
 
 import { Server } from "./Server";
+import { config } from "dotenv";
+config();
 
 const server = new Server();
 server.listen();
