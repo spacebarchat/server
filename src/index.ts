b70adcedbac92a108924a1a29929d17a01903fb9
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-16 13:30:53 +0100

    :sparkles: load .env file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 process.on("uncaughtException", console.error);
 process.on("unhandledRejection", console.error);
 
+import { config } from "dotenv";
+config();
 import { DiscordServer } from "./Server";
 
 const server = new DiscordServer({ port: 3000 });
