488f6f3894959dcb66cb6f7e503871031694f907
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2021-02-21 16:05:03 +0100

    Update index.ts

--- src/routes/api/v8/guilds/index.ts
+++ src/routes/api/v8/guilds/index.ts
@@ -214,8 +214,19 @@ router.delete("/:id", async (req: Request, res: Response) => {
 });
 
 router.get("/:id/bans", async (req: Request, res: Response) => {
+	const guild = await GuildModel.findOne({ id: BigInt(req.params.id) }).exec();
+
+	if (!guild) throw new HTTPError("Guild not found", 404);
 	var bans = await BanModel.find({ guild_id: BigInt(req.params.id) }).exec();
 	return res.json(bans);
+});
+
+router.get("/:id/members", async (req: Request, res: Response) => {
+	const guild = await GuildModel.findOne({ id: BigInt(req.params.id) }).exec();
+
+	if (!guild) throw new HTTPError("Guild not found", 404);
+	var members = await MemberModel.find({ guild_id: BigInt(req.params.id) }).exec();
+	return res.json(members);
 });
 
 router.post("/:id/bans/:userid", check(BanCreateSchema), async (req: Request, res: Response) => {
