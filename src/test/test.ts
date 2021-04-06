5e2400647259535c001830fa0578a6e638bdfca3
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-06 18:57:06 +0200

    :bug: fix test

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,7 @@
 import { getPermission } from "fosscord-server-util";
 
 async function main() {
-	const t = await getPermission(811642917432066048n, 812327318532915201n);
+	const t = await getPermission("811642917432066048", "812327318532915201");
 	console.log(t);
 }
 
