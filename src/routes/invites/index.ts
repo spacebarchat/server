5a0351aa05d141261459f54fcd3c03c3d139db4b
xnacly <matteogropp@gmail.com>
2021-04-08 17:53:57 +0200

    finished /invite route #21

--- src/routes/invites/index.ts
+++ src/routes/invites/index.ts
@@ -1,4 +1,28 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { getPermission, InviteModel, toObject } from "fosscord-server-util";
+import { HTTPError } from "lambert-server";
 const router: Router = Router();
+
+router.get("/:invite_code", async (req: Request, res: Response) => {
+	const { invite_code: code } = req.params;
+
+	const invite = await InviteModel.findOne({ code }).exec();
+
+	if (!invite) throw new HTTPError("Unknown Invite", 404);
+	res.status(200).send({ invite: toObject(invite) });
+});
+
+router.delete("/:invite_code", async (req: Request, res: Response) => {
+	const { invite_code: code } = req.params;
+	const invite = await InviteModel.findOne({ code }).exec();
+
+	if (!invite) throw new HTTPError("Unknown Invite", 404);
+
+	const { guild_id, channel_id } = invite;
+	const perms = await getPermission(req.user_id, guild_id, channel_id);
+
+	if (!perms.has("MANAGE_GUILD") || !perms.has("MANAGE_CHANNELS")) throw new HTTPError("You aren't allow", 401);
+	res.status(200).send({ invite: toObject(invite) });
+});
 
 export default router;
