d7cf341320fb2e1af5d1e40e67c2fed2d65d3a5c
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-24 14:30:48 +0200

    :bug: fix Webhook model

--- src/util/String.ts
+++ src/util/String.ts
@@ -1,6 +1,7 @@
 import { DOUBLE_WHITE_SPACE, SPECIAL_CHAR } from "./Regex";
 
-export function trimSpecial(str?: string) {
+export function trimSpecial(str?: string): string {
+	// @ts-ignore
 	if (!str) return;
 	return str.replace(SPECIAL_CHAR, "").replace(DOUBLE_WHITE_SPACE, " ").trim();
 }
