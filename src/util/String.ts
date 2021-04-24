2c39b659a59bb308a1221c1ab949f774ed195e7c
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-04-24 11:14:17 +0200

    :art: String trimSpecial pass string OR undefined

--- src/util/String.ts
+++ src/util/String.ts
@@ -1,5 +1,6 @@
 import { DOUBLE_WHITE_SPACE, SPECIAL_CHAR } from "./Regex";
 
-export function trimSpecial(str: string) {
+export function trimSpecial(str?: string) {
+	if (!str) return;
 	return str.replace(SPECIAL_CHAR, "").replace(DOUBLE_WHITE_SPACE, " ").trim();
 }
