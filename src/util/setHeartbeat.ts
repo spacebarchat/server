372bfd9beb88269d6578ecf9b478b54ced89bca8
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-05-07 22:52:40 +0200

    :bug: fix identify schema

--- src/util/setHeartbeat.ts
+++ src/util/setHeartbeat.ts
@@ -1,3 +1,4 @@
+import { CLOSECODES } from "./Constants";
 import WebSocket from "./WebSocket";
 
 // TODO: make heartbeat timeout configurable
@@ -5,6 +6,6 @@ export function setHeartbeat(socket: WebSocket) {
 	if (socket.heartbeatTimeout) clearTimeout(socket.heartbeatTimeout);
 
 	socket.heartbeatTimeout = setTimeout(() => {
-		return socket.close(4009);
+		return socket.close(CLOSECODES.Session_timed_out);
 	}, 1000 * 45);
 }
