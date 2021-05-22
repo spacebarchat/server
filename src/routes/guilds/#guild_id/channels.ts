4715ef1eab99f8158b4ddaa0b9b7bc9a61d0e337
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-05-22 17:40:10 +0200

    :sparkles: createChannel()

--- src/routes/guilds/#guild_id/channels.ts
+++ src/routes/guilds/#guild_id/channels.ts
@@ -13,6 +13,7 @@ import { HTTPError } from "lambert-server";
 import { ChannelModifySchema } from "../../../schema/Channel";
 import { emitEvent } from "../../../util/Event";
 import { check } from "../../../util/instanceOf";
+import { createChannel } from "../../../util/Channel";
 const router = Router();
 
 router.get("/", async (req, res) => {
@@ -26,41 +27,7 @@ router.post("/", check(ChannelModifySchema), async (req, res) => {
 	const { guild_id } = req.params;
 	const body = req.body as ChannelModifySchema;
 
-	if (!body.permission_overwrites) body.permission_overwrites = [];
-	if (!body.topic) body.topic = "";
-	if (!body.rate_limit_per_user) body.rate_limit_per_user = 0;
-
-	switch (body.type) {
-		case ChannelType.DM:
-		case ChannelType.GROUP_DM:
-			throw new HTTPError("You can't create a dm channel in a guild");
-		// TODO:
-		case ChannelType.GUILD_STORE:
-			throw new HTTPError("Not yet supported");
-		case ChannelType.GUILD_NEWS:
-		// TODO: check if guild is community server
-	}
-
-	if (body.parent_id) {
-		const exists = await ChannelModel.findOne({ id: body.parent_id }, { guild_id: true }).exec();
-		if (!exists) throw new HTTPError("Parent id channel doesn't exist", 400);
-		if (exists.guild_id !== guild_id) throw new HTTPError("The category channel needs to be in the guild");
-	}
-
-	const guild = await GuildModel.findOne({ id: guild_id }, { id: true }).exec();
-	if (!guild) throw new HTTPError("Guild not found", 404);
-
-	const channel = {
-		...body,
-		id: Snowflake.generate(),
-		created_at: new Date(),
-		guild_id,
-		recipients: null
-	};
-
-	await new ChannelModel(channel).save();
-
-	await emitEvent({ event: "CHANNEL_CREATE", data: channel, guild_id } as ChannelCreateEvent);
+	const channel = await createChannel({ ...body, guild_id }, req.user_id);
 
 	res.json(channel);
 });
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,3 @@
-import { getPermission } from "@fosscord/server-util";
+import { Snowflake } from "@fosscord/server-util";
 
-async function main() {
-	const t = await getPermission("811642917432066048", "812327318532915201");
-	console.log(t);
-}
-
-main();
+console.log(Snowflake.deconstruct("0"));
