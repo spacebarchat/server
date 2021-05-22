e874a946af87fe213ccd0182b44f144f224d0249
Bloom Sirenix <66263341+bloomsirenix@users.noreply.github.com>
2021-05-22 23:30:08 +0200

    Added password check
    :heavy_check_mark: Only works if user types right password
    :no_entry_sign: No more kids getting there account deleted by there brother

--- src/routes/users/@me/delete.ts
+++ src/routes/users/@me/delete.ts
@@ -1,20 +1,29 @@
 import { Router, Request, Response } from "express";
-import { UserModel, toObject } from "@fosscord/server-util";
+import { UserModel,UserDocument, toObject } from "@fosscord/server-util";
 import { getPublicUser } from "../../../util/User";
 import { HTTPError } from "lambert-server";
 import { UserUpdateSchema } from "../../../schema/User";
 import { check } from "../../../util/instanceOf";
 import { db } from "@fosscord/server-util";
+import bcrypt from "bcrypt";
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
-	// TODO:
- //console.log(req.headers);
- await UserModel.deleteOne({id: req.user_id}).exec()
+
+	const user = await UserModel.findOne(
+		{ id: req.user_id },
+		
+	).exec();  //User object
 	
-	//await usermodel.save();
+	let correctpass = await bcrypt.compare(req.body.password,user!.user_data.hash) //Not sure if user typed right password :/
+    if(correctpass){
+	await UserModel.deleteOne({id: req.user_id}).exec() //Yeetus the feetus user deletus
 
 	res.sendStatus(204);
+	}
+	else{
+		res.sendStatus(204);	
+	}
 });
 
 export default router;
