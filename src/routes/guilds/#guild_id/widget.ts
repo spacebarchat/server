f10df31f5c9b99300a54a1c45d45269a96a16a92
Umimaso <git@umimaso.com>
2021-06-19 14:50:11 +0100

    feat: add widget endpoints
    Implemented the four widget related endpoints of the api. Partial user
    object being returned as part of the widget.json endpoint [1] is an
    intentional choice related to privacy [2].
    
    The widget.json endpoint will require additional changes upon completion
    of other work. Member details will need to return extra key/values for
    connected users to voice channels. An additional avatar_url value will
    hold an unique avatar url for the user + guild, fetched via a CDN
    endpoint widget-avatars.
    
    New dependencies `canvas` and `image-size`. Canvas is used to create the
    widget.png endpoint image [3]. Image-size is used to set the canvas'
    size to match the widget template images.
    
    Use regex in determining if a NO_AUTHORIZATION_ROUTES is hit or not.
    
    [1] https://discord.com/developers/docs/resources/guild#get-guild-widget
    [2] https://github.com/discord/discord-api-docs/issues/1287
    [3] https://discord.com/developers/docs/resources/guild#get-guild-widget-image
    
    Closes: #9, #110

---
+++ src/routes/guilds/#guild_id/widget.ts
@@ -0,0 +1,39 @@
+import { Request, Response, Router } from "express";
+import { getPermission, GuildModel } from "@fosscord/server-util";
+import { HTTPError } from "lambert-server";
+import { check } from "../../../util/instanceOf";
+import { WidgetModifySchema } from "../../../schema/Widget";
+
+const router: Router = Router();
+
+// https://discord.com/developers/docs/resources/guild#get-guild-widget-settings
+router.get("/", async (req: Request, res: Response) => {
+	const { guild_id } = req.params;
+
+	const perms = await getPermission(req.user_id, guild_id);
+	perms.hasThrow("MANAGE_GUILD");
+
+	const guild = await GuildModel.findOne({ id: guild_id }).exec();
+	if (!guild) throw new HTTPError("Guild not found", 404);
+
+	return res.json({ enabled: guild.widget_enabled || false, channel_id: guild.widget_channel_id || null});
+});
+
+// https://discord.com/developers/docs/resources/guild#modify-guild-widget
+router.patch("/", check(WidgetModifySchema), async (req: Request, res: Response) => {
+	const body = req.body as WidgetModifySchema;
+	const { guild_id } = req.params;
+
+	const perms = await getPermission(req.user_id, guild_id);
+	perms.hasThrow("MANAGE_GUILD");
+
+	const guild = await GuildModel.findOne({ id: guild_id }).exec();
+	if (!guild) throw new HTTPError("Guild not found", 404);
+
+	await GuildModel.updateOne({ id: guild_id }, { widget_enabled: body['enabled'], widget_channel_id: body['channel_id'] }).exec();
+	// Widget invite for the widget_channel_id gets created as part of the /guilds/{guild.id}/widget.json request
+
+	return res.json(body);
+});
+
+export default router;
