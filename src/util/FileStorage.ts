7b84ef1aa027cf9a0defc90044012e625fe2e427
Joaquim Peixoto <jhcpeixoto@outlook.pt>
2021-06-25 20:20:00 +0100

    FIX MKDIR

--- src/util/FileStorage.ts
+++ src/util/FileStorage.ts
@@ -4,27 +4,27 @@ import { join } from "path";
 import "missing-native-js-functions";
 
 export class FileStorage implements Storage {
-	async get(path: string): Promise<Buffer | null> {
-		path = join(process.env.STORAGE_LOCATION || "", path);
-		try {
-			const file = await fs.readFile(path);
-			// @ts-ignore
-			return file;
-		} catch (error) {
-			return null;
-		}
-	}
+  async get(path: string): Promise<Buffer | null> {
+    path = join(process.env.STORAGE_LOCATION || "", path);
+    try {
+      const file = await fs.readFile(path);
+      // @ts-ignore
+      return file;
+    } catch (error) {
+      return null;
+    }
+  }
 
-	async set(path: string, value: any) {
-		path = join(process.env.STORAGE_LOCATION || "", path);
-		const dir = path.split("/").slice(0, -1).join("/");
-		await fs.mkdir(dir, { recursive: true }).caught();
+  async set(path: string, value: any) {
+    path = join(process.env.STORAGE_LOCATION || "", path).replace(/[\\]/g, "/");
+    const dir = path.split("/").slice(0, -1).join("/");
+    await fs.mkdir(dir, { recursive: true }).caught();
 
-		return fs.writeFile(path, value, { encoding: "binary" });
-	}
+    return fs.writeFile(path, value, { encoding: "binary" });
+  }
 
-	async delete(path: string) {
-		path = join(process.env.STORAGE_LOCATION || "", path);
-		await fs.unlink(path);
-	}
+  async delete(path: string) {
+    path = join(process.env.STORAGE_LOCATION || "", path);
+    await fs.unlink(path);
+  }
 }
