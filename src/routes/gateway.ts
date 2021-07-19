7dab8f9435792c62f8e1e2b4d403f3ab71e8b2f1
Diego Magdaleno <38844659+DiegoMagdaleno@users.noreply.github.com>
2021-07-19 13:17:33 -0500

    [Gateway Endpoint] Encode response as JSON

--- src/routes/gateway.ts
+++ src/routes/gateway.ts
@@ -5,7 +5,8 @@ const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
 	const { endpoint } = Config.get().gateway;
-	res.send({ url: endpoint || process.env.GATEWAY || "ws://localhost:3002" });
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({ url: endpoint || process.env.GATEWAY || "ws://localhost:3002" }));
 });
 
 export default router;
