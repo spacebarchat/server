a95d7b7ce5b6712f1eaec10ed9de6d39d72de3a7
Diego Magdaleno <38844659+DiegoMagdaleno@users.noreply.github.com>
2021-07-21 14:47:07 -0500

    [Fix] All messages should have edited_timestamp even if null

--- src/routes/gateway.ts
+++ src/routes/gateway.ts
@@ -5,8 +5,7 @@ const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
 	const { endpoint } = Config.get().gateway;
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify({ url: endpoint || process.env.GATEWAY || "ws://localhost:3002" }));
+	res.json({ url: endpoint || process.env.GATEWAY || "ws://localhost:3002" });
 });
 
 export default router;
