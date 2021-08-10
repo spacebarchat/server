54f55704a7cd521cb70aa26e86ad62793fddb541
xnacly <matteogropp@protonmail.com>
2021-08-10 20:55:47 +0200

    added disable route & check if user is disabled

--- src/routes/users/@me/disable.ts
+++ src/routes/users/@me/disable.ts
@@ -1,10 +1,20 @@
+import { UserModel } from "@fosscord/server-util";
 import { Router, Response, Request } from "express";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
-router.post("/", (req: Request, res: Response) => {
-	// TODO:
-	res.sendStatus(204);
+router.post("/", async (req: Request, res: Response) => {
+	const user = await UserModel.findOne({ id: req.user_id }).exec(); //User object
+
+	let correctpass = await bcrypt.compare(req.body.password, user!.user_data.hash); //Not sure if user typed right password :/
+	if (correctpass) {
+		await UserModel.updateOne({ id: req.user_id }, { disabled: true }).exec();
+
+		res.sendStatus(204);
+	} else {
+		res.status(400).json({ message: "Password does not match", code: 50018 });
+	}
 });
 
 export default router;
