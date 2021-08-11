27d0e94995cd924d80258b45cf8d60537255be88
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-11 19:45:02 +0200

    :sparkles: use rabbitmq for event transmission

--- src/util/Event.ts
+++ src/util/Event.ts
@@ -1,13 +1,25 @@
-import { Event, EventModel } from "@fosscord/server-util";
+import { Config, Event, EventModel, rabbitCon, rabbitCh } from "@fosscord/server-util";
 
 export async function emitEvent(payload: Omit<Event, "created_at">) {
-	const obj = {
-		created_at: new Date(), // in seconds
-		...payload,
-	};
-	// TODO: bigint isn't working
+	if (rabbitCon) {
+		const id = (payload.channel_id || payload.user_id || payload.guild_id) as string;
+		if (!id) console.error("event doesn't contain any id", payload);
+		const data = typeof payload.data === "object" ? JSON.stringify(payload.data) : payload.data; // use rabbitmq for event transmission
 
-	return await new EventModel(obj).save();
+		// assertQueue isn't needed, because a queue will automatically created if it doesn't exist
+		const successful = rabbitCh.sendToQueue(id, Buffer.from(`${data}`), { type: payload.event });
+		if (!successful) throw new Error("failed to send event");
+	} else {
+		// use mongodb for event transmission
+		// TODO: use event emitter for local server bundle
+		const obj = {
+			created_at: new Date(), // in seconds
+			...payload
+		};
+		// TODO: bigint isn't working
+
+		return await new EventModel(obj).save();
+	}
 }
 
 export async function emitAuditLog(payload: any) {}
