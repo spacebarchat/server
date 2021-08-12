63c1956dd9c0f2b362f958ce4952b8308f267999
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-12 17:43:58 +0200

    :bug: fix rabbit mq -> fanout instead of work queue

--- src/util/Event.ts
+++ src/util/Event.ts
@@ -5,10 +5,10 @@ export async function emitEvent(payload: Omit<Event, "created_at">) {
 		const id = (payload.channel_id || payload.user_id || payload.guild_id) as string;
 		if (!id) console.error("event doesn't contain any id", payload);
 		const data = typeof payload.data === "object" ? JSON.stringify(payload.data) : payload.data; // use rabbitmq for event transmission
-		await RabbitMQ.channel?.assertQueue(id);
+		await RabbitMQ.channel?.assertExchange(id, "fanout", { durable: false });
 
 		// assertQueue isn't needed, because a queue will automatically created if it doesn't exist
-		const successful = RabbitMQ.channel?.sendToQueue(id, Buffer.from(`${data}`), { type: payload.event });
+		const successful = RabbitMQ.channel?.publish(id, "", Buffer.from(`${data}`), { type: payload.event });
 		if (!successful) throw new Error("failed to send event");
 	} else {
 		// use mongodb for event transmission
