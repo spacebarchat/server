a467c863993d2513a605441e68eed7cd15e53923
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-17 22:41:38 +0200

    :sparkles: mention regex

--- util/src/util/Regex.ts
+++ util/src/util/Regex.ts
@@ -1,3 +1,7 @@
 export const DOUBLE_WHITE_SPACE = /\s\s+/g;
 export const SPECIAL_CHAR = /[@#`:\r\n\t\f\v\p{C}]/gu;
 export const CHANNEL_MENTION = /<#(\d+)>/g;
+export const USER_MENTION = /<@!?(\d+)>/g;
+export const ROLE_MENTION = /<@&(\d+)>/g;
+export const EVERYONE_MENTION = /@everyone/g;
+export const HERE_MENTION = /@here/g;
