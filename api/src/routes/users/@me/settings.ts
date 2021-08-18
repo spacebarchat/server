9d0dcae5621d999100c8d850e373cf96f018b32b
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-18 11:53:03 +0200

    :ambulance: user settings route

--- api/src/routes/users/@me/settings.ts
+++ api/src/routes/users/@me/settings.ts
@@ -1,9 +1,15 @@
 import { Router, Response, Request } from "express";
+import { UserModel, UserSettings } from "@fosscord/util";
+import { check } from "../../../util/instanceOf";
+import { UserSettingsSchema } from "../../../schema/User";
 
 const router = Router();
 
-router.patch("/", (req: Request, res: Response) => {
-	// TODO:
+router.patch("/", check(UserSettingsSchema), async (req: Request, res: Response) => {
+	const body = req.body as UserSettings;
+
+	await UserModel.updateOne({ id: req.user_id }, body).exec();
+
 	res.sendStatus(204);
 });
 
