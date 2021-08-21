0c20d2dc1561dda054bcb799bed29bf36077ba10
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-08-21 22:30:42 +0300

    Fix content security policy policy

--- api/src/middlewares/CORS.ts
+++ api/src/middlewares/CORS.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 
 export function CORS(req: Request, res: Response, next: NextFunction) {
 	res.set("Access-Control-Allow-Origin", "*");
-	// TODO: use better CSP policy
+	// TODO: use better CSP
 	res.set(
 		"Content-security-policy",
 		"default-src *  data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline';"
