a2ec99cf281801d2e7ad2c7f8db560088aa41355
FuflyPaladium <61084278+FuflyPaladium@users.noreply.github.com>
2021-08-29 10:43:36 +0200

    request url with "/?with_analytics_token=true" example
    its mainly required by developers apps

--- api/src/routes/users/@me/index.ts
+++ api/src/routes/users/@me/index.ts
@@ -4,10 +4,41 @@ import { getPublicUser } from "../../../util/User";
 import { UserModifySchema } from "../../../schema/User";
 import { check } from "../../../util/instanceOf";
 import { handleFile } from "../../../util/cdn";
+import { regexpCode } from "ajv/dist/compile/codegen";
+import jwt from "jsonwebtoken";
+import { Config } from "@fosscord/util";
 
 const router: Router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
+	console.log(req.url);
+	if(req.url == "/?with_analytics_token=true") {
+		const userWithAnalyticsToken = {
+			accent_color: null,
+			analytics_token: await generateToken(req.user_id),
+			avatar: (await getPublicUser(req.user_id)).avatar,
+			banner: (await getPublicUser(req.user_id)).banner,
+			banner_color: null, //TODO ?
+			bio: (await getPublicUser(req.user_id)).bio,
+			discriminator: (await getPublicUser(req.user_id)).discriminator,
+			email: (await getPublicUser(req.user_id)).email,
+			flags: (await getPublicUser(req.user_id)).flags,
+			id: req.user_id,
+			locale: "fr", //TODO ?
+			mfa_enabled: (await getPublicUser(req.user_id)).mfa_enabled,
+			nsfw_allowed: (await getPublicUser(req.user_id)).nsfw_allowed,
+			phone: (await getPublicUser(req.user_id)).phone,
+			premium_type: (await getPublicUser(req.user_id)).premium_type,
+			premium_usage_flags: 0, //TODO ?
+			public_flags: (await getPublicUser(req.user_id)).public_flags,
+			purchased_flags: 0, //TODO ?
+			username: (await getPublicUser(req.user_id)).username,
+			verified: (await getPublicUser(req.user_id)).verified, //without verified email you can't access to applications
+		};
+		res.json(userWithAnalyticsToken);
+		res.status(200);
+		return;
+	}
 	res.json(await getPublicUser(req.user_id));
 });
 
@@ -40,9 +71,27 @@ router.patch("/", check(UserModifySchema), async (req: Request, res: Response) =
 
 	const user = await UserModel.findOneAndUpdate({ id: req.user_id }, body, { projection: UserUpdateProjection, new: true }).exec();
 	// TODO: dispatch user update event
-
 	res.json(toObject(user));
 });
+
+export async function generateToken(id: string) {
+	const iat = Math.floor(Date.now() / 1000);
+	const algorithm = "HS256";
+
+	return new Promise((res, rej) => {
+		jwt.sign(
+			{ id: id, iat },
+			Config.get().security.jwtSecret,
+			{
+				algorithm
+			},
+			(err, token) => {
+				if (err) return rej(err);
+				return res(token);
+			}
+		);
+	});
+}
 
 export default router;
 // {"message": "Invalid two-factor code", "code": 60008}
