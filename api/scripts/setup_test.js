d107c66358592336d9f6ba8d263b645c242b4c1e
xnacly <matteogropp@protonmail.com>
2021-08-30 16:21:54 +0200

    added setup for jest tests

--- api/scripts/setup_test.js
+++ api/scripts/setup_test.js
@@ -1,1 +1,13 @@
-// TODO: start api
+const fs = require("fs");
+const { FosscordServer } = require("../dist/Server");
+const Server = new FosscordServer({ port: 3001 });
+(async () => {
+	try {
+		fs.unlinkSync(`${__dirname}/database.db`);
+	} catch {}
+	return await Server.start();
+})();
+
+// afterAll(async () => {
+// 	return await Server.stop();
+// });
