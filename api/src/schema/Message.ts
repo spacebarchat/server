3f66c101789f9a6d065490d3e3265c764698113f
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-31 17:58:47 +0200

    :bug: db query fixes

--- api/src/schema/Message.ts
+++ api/src/schema/Message.ts
@@ -81,7 +81,7 @@ export interface MessageCreateSchema {
 		message_id: string;
 		channel_id: string;
 		guild_id?: string;
-		fail_if_not_exists: boolean;
+		fail_if_not_exists?: boolean;
 	};
 	payload_json?: string;
 	file?: any;
