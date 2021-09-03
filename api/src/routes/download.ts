363f1c7a6df8a13006a9c7da79c2816ae5ea69f7
Stilic <63605602+Stilic@users.noreply.github.com>
2021-09-03 22:43:12 +0200

    Create download.ts

---
+++ api/src/routes/download.ts
@@ -0,0 +1,10 @@
+import { Config } from "@fosscord/util";
+import { Router, Response, Request } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+	res.send("We don't support donwloads at the moment");
+});
+
+export default router;
