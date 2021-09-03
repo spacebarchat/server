7beee199064fc1fe1c8553b0f6531d0c652134a7
xnacly <matteogropp@protonmail.com>
2021-09-03 21:40:44 +0200

    added first draft of /discoverable-guilds

---
+++ api/src/routes/discoverable-guilds.ts
@@ -0,0 +1,14 @@
+import { Guild } from "@fosscord/util";
+import { Router, Request, Response } from "express";
+import { In } from "typeorm";
+
+const router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
+	const { limit } = req.params;
+
+	const guilds = await Guild.find({ where: { features: "DISCOVERABLE" } }); //, take: Math.abs(Number(limit)) });
+	res.send({ guilds: guilds });
+});
+
+export default router;
