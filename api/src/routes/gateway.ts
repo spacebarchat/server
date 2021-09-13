324eb7f1dd577b863b6dfc1674c6f25e1c0980df
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-09-13 17:51:37 +0200

    :bug: fix fosscord.js

--- api/src/routes/gateway.ts
+++ api/src/routes/gateway.ts
@@ -9,4 +9,18 @@ router.get("/", route({}), (req: Request, res: Response) => {
 	res.json({ url: endpoint || process.env.GATEWAY || "ws://localhost:3002" });
 });
 
+router.get("/bot", route({}), (req: Request, res: Response) => {
+	const { endpoint } = Config.get().gateway;
+	res.json({
+		url: endpoint || process.env.GATEWAY || "ws://localhost:3002",
+		shards: 1,
+		session_start_limit: {
+			total: 1000,
+			remaining: 999,
+			reset_after: 14400000,
+			max_concurrency: 1
+		}
+	});
+});
+
 export default router;
