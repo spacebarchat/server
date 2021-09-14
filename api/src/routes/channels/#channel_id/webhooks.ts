a359375b77aef4c0792680149382be18661b8bac
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-09-14 21:16:04 +0200

    fix #128

--- api/src/routes/channels/#channel_id/webhooks.ts
+++ api/src/routes/channels/#channel_id/webhooks.ts
@@ -1,9 +1,21 @@
 import { Router, Response, Request } from "express";
-import { route } from "@fosscord/api";
-import { Channel, Config, getPermission, trimSpecial, Webhook } from "@fosscord/util";
+import { handleFile, route } from "@fosscord/api";
+import {
+	Channel,
+	Config,
+	emitEvent,
+	getPermission,
+	Snowflake,
+	trimSpecial,
+	User,
+	Webhook,
+	WebhooksUpdateEvent,
+	WebhookType
+} from "@fosscord/util";
 import { HTTPError } from "lambert-server";
 import { isTextChannel } from "./messages/index";
 import { DiscordApiErrors } from "@fosscord/util";
+import { generateToken } from "../../auth/login";
 
 const router: Router = Router();
 // TODO: webhooks
@@ -11,13 +23,26 @@ export interface WebhookCreateSchema {
 	/**
 	 * @maxLength 80
 	 */
-	name: string;
-	avatar: string;
+	name?: string;
+	avatar?: string;
 }
+
+router.get("/", route({ permission: "MANAGE_WEBHOOKS" }), async (req, res) => {
+	const webhooks = await Webhook.find({
+		where: { channel_id: req.params.channel_id },
+		select: ["application", "avatar", "channel_id", "guild_id", "id", "token", "type", "user", "source_guild", "name"],
+		relations: ["user", "application", "source_guild"]
+	});
+
+	res.json(webhooks);
+});
 
 // TODO: use Image Data Type for avatar instead of String
 router.post("/", route({ body: "WebhookCreateSchema", permission: "MANAGE_WEBHOOKS" }), async (req: Request, res: Response) => {
-	const channel_id = req.params.channel_id;
+	var { avatar, name } = req.body as WebhookCreateSchema;
+	name = trimSpecial(name) || "Webhook";
+	if (name === "clyde") throw new HTTPError("Invalid name", 400);
+	const { channel_id } = req.params;
 	const channel = await Channel.findOneOrFail({ id: channel_id });
 
 	isTextChannel(channel.type);
@@ -27,11 +52,29 @@ router.post("/", route({ body: "WebhookCreateSchema", permission: "MANAGE_WEBHOO
 	const { maxWebhooks } = Config.get().limits.channel;
 	if (webhook_count > maxWebhooks) throw DiscordApiErrors.MAXIMUM_WEBHOOKS.withParams(maxWebhooks);
 
-	var { avatar, name } = req.body as { name: string; avatar?: string };
-	name = trimSpecial(name);
-	if (name === "clyde") throw new HTTPError("Invalid name", 400);
-
+	const id = Snowflake.generate();
 	// TODO: save webhook in database and send response
+	const webhook = await new Webhook({
+		id,
+		name,
+		avatar: await handleFile(`/icons/${id}`, avatar),
+		user: await User.getPublicUser(req.user_id),
+		guild_id: channel.guild_id,
+		channel_id,
+		token: await generateToken(id),
+		type: WebhookType.Incoming
+	}).save();
+
+	await emitEvent({
+		event: "WEBHOOKS_UPDATE",
+		channel_id,
+		data: {
+			channel_id,
+			guild_id: channel.guild_id
+		}
+	} as WebhooksUpdateEvent);
+
+	return res.json(webhook);
 });
 
 export default router;
