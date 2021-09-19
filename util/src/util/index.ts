705206ec1b020bbae2b92de1e98e8a7609a6c850
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-09-19 18:47:38 +0200

    :art: add orphanedRowAction and cascade onDelete to entities

--- util/src/util/index.ts
+++ util/src/util/index.ts
@@ -1,6 +1,7 @@
 export * from "./ApiError";
 export * from "./BitField";
 export * from "./checkToken";
+export * from "./cdn";
 export * from "./Config";
 export * from "./Constants";
 export * from "./Database";
