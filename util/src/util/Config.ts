39f4aa86da5fa3b7ed5e6ad6eabb19bd486ec324
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-09-25 23:24:16 +0200

    :sparkles: key value config

--- util/src/util/Config.ts
+++ util/src/util/Config.ts
@@ -1,22 +1,66 @@
 import "missing-native-js-functions";
 import { ConfigValue, ConfigEntity, DefaultConfigOptions } from "../entities/Config";
 
-var config: ConfigEntity;
+var config: ConfigValue;
+var pairs: ConfigEntity[];
+
 // TODO: use events to inform about config updates
+// Config keys are separated with _
 
 export const Config = {
 	init: async function init() {
 		if (config) return config;
-		config = (await ConfigEntity.findOne({ id: "0" })) || new ConfigEntity({ id: "0" });
+		pairs = await ConfigEntity.find();
+		config = pairsToConfig(pairs);
 
-		return this.set((config.value || {}).merge(DefaultConfigOptions));
+		return this.set((config || {}).merge(DefaultConfigOptions));
 	},
 	get: function get() {
-		return config.value as ConfigValue;
+		return config;
 	},
 	set: function set(val: Partial<ConfigValue>) {
-		if (!config) return;
-		config.value = val.merge(config?.value || {});
-		return config.save();
+		if (!config || !val) return;
+		config = val.merge(config);
+		console.log(config);
+
+		return applyConfig(config);
 	},
 };
+
+function applyConfig(val: ConfigValue) {
+	async function apply(obj: any, key = ""): Promise<any> {
+		if (typeof obj === "object" && obj !== null)
+			return Promise.all(Object.keys(obj).map((k) => apply(obj[k], key ? `${key}_${k}` : k)));
+
+		let pair = pairs.find((x) => x.key === key);
+		if (!pair) pair = new ConfigEntity();
+
+		pair.key = key;
+		pair.value = obj;
+		return pair.save();
+	}
+
+	return apply(val);
+}
+
+function pairsToConfig(pairs: ConfigEntity[]) {
+	var value: any = {};
+
+	pairs.forEach((p) => {
+		const keys = p.key.split("_");
+		let prev = "";
+		let obj = value;
+		let i = 0;
+
+		for (const key of keys) {
+			if (Number(key) && !obj[prev]) obj = obj[prev] = [];
+			if (i++ === keys.length - 1) obj[key] = p.value;
+			else if (!obj[key]) obj[key] = {};
+
+			prev = key;
+			obj = obj[key];
+		}
+	});
+
+	return value as ConfigValue;
+}
