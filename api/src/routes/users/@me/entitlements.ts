7ae37c53719c782838c5cf77c3ad719ca20d72df
uurgothat <cckhmck@gmail.com>
2021-09-30 18:57:53 +0300

    hotfix for misc routes

--- api/src/routes/users/@me/entitlements.ts
+++ api/src/routes/users/@me/entitlements.ts
@@ -5,7 +5,6 @@ const router = Router();
 
 router.get("/gifts", route({}), (req: Request, res: Response) => {
 	// TODO:
-    //const { locale, primary_only } = req.query;
 	res.json([]).status(200);
 });
 
