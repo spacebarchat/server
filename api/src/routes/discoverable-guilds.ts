c772816fd9037505baebc6f82fb4185ff4d2abf0
The Arcane Brony <myrainbowdash949@gmail.com>
2021-10-01 22:43:58 +0200

    Add default value to guild discovery"

--- api/src/routes/discoverable-guilds.ts
+++ api/src/routes/discoverable-guilds.ts
@@ -9,11 +9,10 @@ const router = Router();
 router.get("/", route({}), async (req: Request, res: Response) => {
 	const { limit } = req.params;
 	var showAllGuilds = Config.get().guild.showAllGuildsInDiscovery;
-
 	// ! this only works using SQL querys
 	// TODO: implement this with default typeorm query
 	// const guilds = await Guild.find({ where: { features: "DISCOVERABLE" } }); //, take: Math.abs(Number(limit)) });
-	const guilds = showAllGuilds ? await Guild.find({take: Math.abs(Number(limit))}) : await Guild.find({ where: `"features" LIKE '%COMMUNITY%'`, take: Math.abs(Number(limit)) });
+	const guilds = showAllGuilds ? await Guild.find({take: Math.abs(Number(limit || 20))}) : await Guild.find({ where: `"features" LIKE '%COMMUNITY%'`, take: Math.abs(Number(limit || 20)) });
 	res.send({ guilds: guilds });
 });
 
