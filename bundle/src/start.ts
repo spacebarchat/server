d4ddf3acf06cb2063d4c4128fcda05ba8f2c78ab
uurgothat <cckhmck@gmail.com>
2021-10-04 22:01:08 +0300

    Rainbow the logs

--- bundle/src/start.ts
+++ bundle/src/start.ts
@@ -1,10 +1,28 @@
 // process.env.MONGOMS_DEBUG = "true";
 import cluster from "cluster";
 import os from "os";
+import { red, bold, yellow, cyan } from "chalk";
 import { initStats } from "./stats";
 
 // TODO: add tcp socket event transmission
 const cores = 1 || Number(process.env.threads) || os.cpus().length;
+const commit = require('child_process').execSync('git rev-parse HEAD').toString().trim();
+
+console.log(bold(`
+███████  ██████  ███████ ███████  ██████  ██████  ██████  ██████  
+██      ██    ██ ██      ██      ██      ██    ██ ██   ██ ██   ██ 
+█████   ██    ██ ███████ ███████ ██      ██    ██ ██████  ██   ██ 
+██      ██    ██      ██      ██ ██      ██    ██ ██   ██ ██   ██ 
+██       ██████  ███████ ███████  ██████  ██████  ██   ██ ██████  
+           
+
+		fosscord-server | ${yellow(`Pre-relase (${commit.slice(0, 7)})`)}
+
+Current commit: ${cyan(commit)} (${yellow(commit.slice(0, 7))})
+`))
+
+
+
 
 if (cluster.isMaster && !process.env.masterStarted) {
 	process.env.masterStarted = "true";
@@ -24,7 +42,7 @@ if (cluster.isMaster && !process.env.masterStarted) {
 
 		cluster.on("exit", (worker: any, code: any, signal: any) => {
 			console.log(
-				`[Worker] died with pid: ${worker.process.pid} , restarting ...`
+				`[Worker] ${red(`died with pid: ${worker.process.pid} , restarting ...`)}`
 			);
 			cluster.fork();
 		});
