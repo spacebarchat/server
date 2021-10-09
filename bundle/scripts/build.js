c217e60ff6d7adfef96548e88a382a55c711bde3
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-10-09 18:06:02 +0200

    :bug: fix build.js?

--- bundle/scripts/build.js
+++ bundle/scripts/build.js
@@ -89,7 +89,7 @@ function walk(dir) {
 	var results = [];
 	var list = fs.readdirSync(dir);
 	list.forEach(function (file) {
-		file = dir + "/" + file;
+		file = path.join(dir, file);
 		var stat = fs.statSync(file);
 		if (stat && stat.isDirectory()) {
 			/* Recurse into a subdirectory */
