4722dd5d978e39b203c4ded9c56697467b9f7ece
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-10-10 12:35:46 +0200

    :bug: fix tsc compiler

--- bundle/scripts/build.js
+++ bundle/scripts/build.js
@@ -2,18 +2,32 @@ const { execSync } = require("child_process");
 const path = require("path");
 const fse = require("fs-extra");
 
-const api = path.join(__dirname, "..", "..", "api");
-const dist = path.join(__dirname, "..", "dist");
+fse.copySync(path.join(__dirname, "..", "..", "api", "assets"), path.join(__dirname, "..", "dist", "api", "assets"));
+fse.copySync(
+	path.join(__dirname, "..", "..", "api", "client_test"),
+	path.join(__dirname, "..", "dist", "api", "client_test")
+);
+fse.copySync(path.join(__dirname, "..", "..", "api", "locales"), path.join(__dirname, "..", "dist", "api", "locales"));
+fse.copySync(path.join(__dirname, "..", "..", "api", "src"), path.join(__dirname, "..", "dist", "api", "src"));
+fse.copySync(path.join(__dirname, "..", "..", "util", "src"), path.join(__dirname, "..", "dist", "util", "src"));
+fse.copySync(path.join(__dirname, "..", "..", "cdn", "src"), path.join(__dirname, "..", "dist", "cdn", "src"));
+fse.copySync(path.join(__dirname, "..", "..", "gateway", "src"), path.join(__dirname, "..", "dist", "gateway", "src"));
+fse.copySync(path.join(__dirname, "..", "..", "bundle", "src"), path.join(__dirname, "..", "dist", "bundle", "src"));
 
-fse.copySync(path.join(api, "assets"), path.join(dist, "api", "assets"));
-fse.copySync(path.join(api, "client_test"), path.join(dist, "api", "client_test"));
-fse.copySync(path.join(api, "locales"), path.join(dist, "api", "locales"));
+console.log("Copying   src files done");
+console.log("Compiling src files ...");
 
 console.log(
-	execSync("node " + path.join(__dirname, "..", "node_modules", "typescript", "lib", "tsc.js") + " -p .", {
-		cwd: path.join(__dirname, ".."),
-		shell: true,
-		env: process.env,
-		encoding: "utf8",
-	})
+	execSync(
+		"node " +
+			path.join(__dirname, "..", "node_modules", "typescript", "lib", "tsc.js") +
+			" -p " +
+			path.join(__dirname, ".."),
+		{
+			cwd: path.join(__dirname, ".."),
+			shell: true,
+			env: process.env,
+			encoding: "utf8",
+		}
+	)
 );
