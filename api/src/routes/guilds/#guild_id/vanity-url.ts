4b811d3b517801733d47553a5ba997a4d88ee369
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-10-10 18:28:38 +0200

    :bug: fix vanity url

--- api/src/routes/guilds/#guild_id/vanity-url.ts
+++ api/src/routes/guilds/#guild_id/vanity-url.ts
@@ -10,7 +10,7 @@ const InviteRegex = /\W/g;
 router.get("/", route({ permission: "MANAGE_GUILD" }), async (req: Request, res: Response) => {
 	const { guild_id } = req.params;
 
-	const invite = await Invite.findOne({ where: {guild_id: guild_id, vanity_url: true} });
+	const invite = await Invite.findOne({ where: { guild_id: guild_id, vanity_url: true } });
 	if (!invite) return res.json({ code: null });
 
 	return res.json({ code: invite.code, uses: invite.uses });
@@ -35,15 +35,7 @@ router.patch("/", route({ body: "VanityUrlSchema", permission: "MANAGE_GUILD" })
 
 	const { id } = await Channel.findOneOrFail({ guild_id, type: ChannelType.GUILD_TEXT });
 
-	Promise.all([
-		new Invite({
-			code: code,
-			uses: 0,
-			created_at: new Date(),
-			guild_id,
-			channel_id: id
-		}).save()
-	]);
+	await Invite.update({ vanity_url: true, guild_id }, { code: code, channel_id: id });
 
 	return res.json({ code: code });
 });
