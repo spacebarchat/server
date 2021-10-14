35ef010ba9a33776e858d1116dce568ce5a9c41e
Hayden Young <hi@hbjy.dev>
2021-10-14 18:47:05 +0100

    feat: implement a database check on the healthz and readyz probes, remove /-/metrics

--- api/src/routes/-/healthz.ts
+++ api/src/routes/-/healthz.ts
@@ -1,10 +1,17 @@
 import { Router, Response, Request } from "express";
 import { route } from "@fosscord/api";
+import { getConnection } from "typeorm";
 
 const router = Router();
 
 router.get("/", route({}), (req: Request, res: Response) => {
-	res.sendStatus(200);
+	try {
+		// test that the database is alive & responding
+		getConnection();
+		return res.sendStatus(200);
+	} catch(e) {
+		res.sendStatus(503);
+	}
 });
 
 export default router;
