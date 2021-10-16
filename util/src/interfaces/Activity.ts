bd0b941feaff6b414caf2c90cb1f1d0dce2e8106
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-10-17 00:38:56 +0200

    :sparkles: added session + memberlist event

--- util/src/interfaces/Activity.ts
+++ util/src/interfaces/Activity.ts
@@ -1,37 +1,38 @@
 export interface Activity {
-	name: string;
-	type: ActivityType;
-	url?: string;
-	created_at?: Date;
+	name: string; // the activity's name
+	type: ActivityType; // activity type // TODO: check if its between range 0-5
+	url?: string; // stream url, is validated when type is 1
+	created_at?: number; // unix timestamp of when the activity was added to the user's session
 	timestamps?: {
-		start?: number;
-		end?: number;
-	}[];
-	application_id?: string;
+		// unix timestamps for start and/or end of the game
+		start: number;
+		end: number;
+	};
+	application_id?: string; // application id for the game
 	details?: string;
 	state?: string;
 	emoji?: {
 		name: string;
 		id?: string;
-		amimated?: boolean;
+		animated: boolean;
 	};
 	party?: {
 		id?: string;
-		size?: [number, number];
+		size?: [number]; // used to show the party's current and maximum size // TODO: array length 2
 	};
 	assets?: {
-		large_image?: string;
-		large_text?: string;
-		small_image?: string;
-		small_text?: string;
+		large_image?: string; // the id for a large asset of the activity, usually a snowflake
+		large_text?: string; // text displayed when hovering over the large image of the activity
+		small_image?: string; // the id for a small asset of the activity, usually a snowflake
+		small_text?: string; // text displayed when hovering over the small image of the activity
 	};
 	secrets?: {
-		join?: string;
-		spectate?: string;
-		match?: string;
+		join?: string; // the secret for joining a party
+		spectate?: string; // the secret for spectating a game
+		match?: string; // the secret for a specific instanced match
 	};
 	instance?: boolean;
-	flags?: bigint;
+	flags: string; // activity flags OR d together, describes what the payload includes
 }
 
 export enum ActivityType {
