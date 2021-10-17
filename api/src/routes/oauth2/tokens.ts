3a118841a00e0bc561c2ac6cb45ceb1764dbd62c
uurgothat <cckhmck@gmail.com>
2021-10-17 21:51:19 +0300

    oauth2 route draft to return an empty array

---
+++ api/src/routes/oauth2/tokens.ts
@@ -0,0 +1,12 @@
+//TODO: this is a template for a generic route
+
+import { Router, Request, Response } from "express";
+import { route } from "@fosscord/api";
+const router = Router();
+
+router.get("/",route({}), async (req: Request, res: Response) => {
+    //TODO
+	res.json([]);
+});
+
+export default router;
