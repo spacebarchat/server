a03d470c93bc519ddf9f4f303f5a24e50ce2a78f
developomp <developomp@gmail.com>
2021-10-20 09:15:52 +0900

    prettier formatted /gateway

--- gateway/src/util/SessionUtils.ts
+++ gateway/src/util/SessionUtils.ts
@@ -7,5 +7,7 @@ export function genVoiceToken() {
 }
 
 function genRanHex(size: number) {
-	return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
+	return [...Array(size)]
+		.map(() => Math.floor(Math.random() * 16).toString(16))
+		.join("");
 }
