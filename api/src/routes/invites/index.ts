9e4589503163e8f82788033c439869dc4acc282f
Kuna <65683493+Thesourtimes@users.noreply.github.com>
2021-12-07 19:39:03 +0300

    Update index.ts

--- api/src/routes/invites/index.ts
+++ api/src/routes/invites/index.ts
@@ -15,7 +15,8 @@ router.get("/:code", route({}), async (req: Request, res: Response) => {
 
 router.post("/:code", route({}), async (req: Request, res: Response) => {
 	const { code } = req.params;
-	const { features } = await Guild.findOneOrFail({ id: (await Invite.findOneOrFail({ code })).guild_id});
+        const { guild_id } = await Invite.findOneOrFail({ code })
+	const { features } = await Guild.findOneOrFail({ id: guild_id});
 	const { public_flags } = await User.findOneOrFail({ id: req.user_id });
 	
 	if(features.includes("INTERNAL_EMPLOYEE_ONLY") && (public_flags & 1) !== 1) throw new HTTPError("You are not allowed to join this guild.", 401)
