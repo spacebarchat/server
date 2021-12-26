2d7d69853f7020f37bff9ddf1bf398fbea40f1f2
Erkin Alp Güney <erkinalp9035@gmail.com>
2021-12-26 20:26:15 +0300

    Fix the casing
    Co-authored-by: TheArcaneBrony<myrainbowdash949@gmail.com>

--- bundle/src/start.ts
+++ bundle/src/start.ts
@@ -31,7 +31,7 @@ if (cluster.isMaster) {
 ╚═╝      ╚═════╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝
 
 		fosscord-server | ${yellow(
-			`Pre-Release (${
+			`Pre-release (${
 				commit !== null
 					? commit.slice(0, 7)
 					: "Unknown (Git cannot be found)"
