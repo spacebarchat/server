e82d527283093bb9e3a030a51b618c92839528ad
Thesourtimes <cckhmck@gmail.com>
2022-01-01 20:59:08 +0300

    Fix schemas handler

--- api/src/util/handlers/route.ts
+++ api/src/util/handlers/route.ts
@@ -18,7 +18,7 @@ import Ajv from "ajv";
 import { AnyValidateFunction } from "ajv/dist/core";
 import addFormats from "ajv-formats";
 
-const SchemaPath = path.join(__dirname, "..", "..", "assets", "schemas.json");
+const SchemaPath = path.join(__dirname, "..", "..", "..", "assets", "schemas.json");
 const schemas = JSON.parse(fs.readFileSync(SchemaPath, { encoding: "utf8" }));
 
 export const ajv = new Ajv({
