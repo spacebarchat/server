7ba65f8f669b957290411ad1513cd2da0923cabd
Featyre <twooter.0g179@simplelogin.co>
2022-01-23 23:55:43 +0800

    Partial integration of categories and discovery

---
+++ util/src/entities/Categories.ts
@@ -0,0 +1,36 @@
+import { PrimaryColumn, Column, Entity} from "typeorm";
+import { BaseClassWithoutId } from "./BaseClass";
+
+// TODO: categories:
+// [{
+// 	"id": 16,
+// 	"default": "Anime & Manga",
+// 	"localizations": {
+// 			"de": "Anime & Manga",
+// 			"fr": "Anim\u00e9s et mangas",
+// 			"ru": "\u0410\u043d\u0438\u043c\u0435 \u0438 \u043c\u0430\u043d\u0433\u0430"
+// 		}
+// 	},
+// 	"is_primary": false/true
+// }]
+
+@Entity("categories")
+export class Categories extends BaseClassWithoutId { // Not using snowflake
+    
+    @PrimaryColumn()
+	id: number;
+
+    @Column()
+    default: string;
+
+    @Column({ type: "simple-json", nullable: false })
+    localizations: string;
+
+    @Column()
+    is_primary: boolean;
+
+}
+
+export interface DefaultCategoryValue { // TODO: Load Default Discord Categories
+
+}
