821470cc41f7922b3a5f471988e344332a34068d
Featyre <twooter.0g179@simplelogin.co>
2022-01-26 08:58:36 +0800

    Dev portal + categories load db

--- api/src/routes/experiments.ts
+++ api/src/routes/experiments.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/", route({}), (req: Request, res: Response) => {
 	// TODO:
-	res.send({ fingerprint: "", assignments: [] });
+	res.send({ fingerprint: "", assignments: [], guild_experiments:[] });
 });
 
 export default router;
