3ce20fe01167d59adcb422b5f600470e7d802c11
David Sundberg <91062384+DavidNordicInternational@users.noreply.github.com>
2022-02-15 11:03:03 +0000

    Fixed bugs in Api and Bundle. They should be fixed now.

--- bundle/scripts/benchmark/connections.js
+++ bundle/scripts/benchmark/connections.js
@@ -7,53 +7,53 @@ const threads = Number(process.env.THREADS) || require("os").cpus().length || 1;
 const token = process.env.TOKEN;
 
 if (!token) {
-	console.error("TOKEN env var missing");
-	process.exit();
+  console.error("TOKEN env var missing");
+  process.exit();
 }
 
 if (cluster.isMaster) {
-	for (let i = 0; i < threads; i++) {
-		cluster.fork();
-	}
+  for (let i = 0; i < threads; i++) {
+    cluster.fork();
+  }
 
-	cluster.on("exit", (worker, code, signal) => {
-		console.log(`worker ${worker.process.pid} died`);
-	});
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
 } else {
-	for (let i = 0; i < connections; i++) {
-		connect();
-	}
+  for (let i = 0; i < connections; i++) {
+    connect();
+  }
 }
 
 function connect() {
-	const client = new WebSocket(endpoint);
-	client.on("message", (data) => {
-		data = JSON.parse(data);
+  const client = new WebSocket(endpoint);
+  client.on("message", (data) => {
+    data = JSON.parse(data);
 
-		switch (data.op) {
-			case 10:
-				client.interval = setInterval(() => {
-					client.send(JSON.stringify({ op: 1 }));
-				}, data.d.heartbeat_interval);
+    switch (data.op) {
+      case 10:
+        client.interval = setInterval(() => {
+          client.send(JSON.stringify({ op: 1 }));
+        }, data.d.heartbeat_interval);
 
-				client.send(
-					JSON.stringify({
-						op: 2,
-						d: {
-							token,
-							properties: {},
-						},
-					})
-				);
+        client.send(
+          JSON.stringify({
+            op: 2,
+            d: {
+              token,
+              properties: {},
+            },
+          })
+        );
 
-				break;
-		}
-	});
-	client.once("close", (code, reason) => {
-		clearInterval(client.interval);
-		connect();
-	});
-	client.on("error", (err) => {
-		// console.log(err);
-	});
+        break;
+    }
+  });
+  client.once("close", (code, reason) => {
+    clearInterval(client.interval);
+    connect();
+  });
+  client.on("error", (err) => {
+    // console.log(err);
+  });
 }
