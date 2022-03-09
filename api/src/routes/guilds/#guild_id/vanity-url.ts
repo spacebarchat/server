134d746ebf41fe0682d8525b20c10bf8bcde25f8
Nobody <17956512+n0bodysec@users.noreply.github.com>
2022-03-09 11:35:19 -0300

    fix(api): add vanity urls creation/update (#665)
    * fix(api): add vanity urls creation/update
    
    * refactor(api): multiple vanity urls
    Old vanty urls will not be updated, instead a new one will be created.
    * feat(api): add ALIASABLE_NAMES feature
    
    Reference: https://github.com/fosscord/fosscord-server/issues/407

--- api/src/routes/guilds/#guild_id/vanity-url.ts
+++ api/src/routes/guilds/#guild_id/vanity-url.ts
@@ -9,11 +9,19 @@ const InviteRegex = /\W/g;
 
 router.get("/", route({ permission: "MANAGE_GUILD" }), async (req: Request, res: Response) => {
 	const { guild_id } = req.params;
+	const guild = await Guild.findOneOrFail({ id: guild_id });
 
-	const invite = await Invite.findOne({ where: { guild_id: guild_id, vanity_url: true } });
-	if (!invite) return res.json({ code: null });
+	if (!guild.features.includes("ALIASABLE_NAMES")) {
+		const invite = await Invite.findOne({ where: { guild_id: guild_id, vanity_url: true } });
+		if (!invite) return res.json({ code: null });
 
-	return res.json({ code: invite.code, uses: invite.uses });
+		return res.json({ code: invite.code, uses: invite.uses });
+	} else {
+		const invite = await Invite.find({ where: { guild_id: guild_id, vanity_url: true } });
+		if (!invite || invite.length == 0) return res.json({ code: null });
+
+		return res.json(invite.map((x) => ({ code: x.code, uses: x.uses })));
+	}
 });
 
 export interface VanityUrlSchema {
@@ -24,18 +32,33 @@ export interface VanityUrlSchema {
 	code?: string;
 }
 
-// TODO: check if guild is elgible for vanity url
 router.patch("/", route({ body: "VanityUrlSchema", permission: "MANAGE_GUILD" }), async (req: Request, res: Response) => {
 	const { guild_id } = req.params;
 	const body = req.body as VanityUrlSchema;
 	const code = body.code?.replace(InviteRegex, "");
+
+	const guild = await Guild.findOneOrFail({ id: guild_id });
+	if (!guild.features.includes("VANITY_URL")) throw new HTTPError("Your guild doesn't support vanity urls");
+
+	if (!code || code.length === 0) throw new HTTPError("Code cannot be null or empty");
 
 	const invite = await Invite.findOne({ code });
 	if (invite) throw new HTTPError("Invite already exists");
 
 	const { id } = await Channel.findOneOrFail({ guild_id, type: ChannelType.GUILD_TEXT });
 
-	await Invite.update({ vanity_url: true, guild_id }, { code: code, channel_id: id });
+	await new Invite({
+		vanity_url: true,
+		code: code,
+		temporary: false,
+		uses: 0,
+		max_uses: 0,
+		max_age: 0,
+		created_at: new Date(),
+		expires_at: new Date(),
+		guild_id: guild_id,
+		channel_id: id
+	}).save();
 
 	return res.json({ code: code });
 });
