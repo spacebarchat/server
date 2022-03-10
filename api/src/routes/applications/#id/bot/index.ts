d6d61b7f800049d0a0a88430d920c96304934912
Featyre <twooter.0g179@simplelogin.co>
2022-03-10 10:46:10 +0800

    update bot_id after creating bot (not working)

--- api/src/routes/applications/#id/bot/index.ts
+++ api/src/routes/applications/#id/bot/index.ts
@@ -8,11 +8,15 @@ router.post("/", route({}), async (req: Request, res: Response) => {
 
 	const id = req.params.id
 
-	const application = await Application.findOneOrFail({ select: ["name" ], where: `"id" = ${id}`})
+	const application = await Application.findOneOrFail({ where: {id: id}})
 
 	const name = application.name
  
 	await User.addBot({ name, id, req })
+
+	application.assign({ bot_id: id })
+
+	await Application.update({id}, { bot_id: id })
 
 	res.send([]).status(204);
 });
