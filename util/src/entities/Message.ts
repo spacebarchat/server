764d2801a683e343de0ff88e46053952f8a8ab46
Erkin Alp Güney <erkinalp9035@gmail.com>
2022-04-23 16:37:16 +0300

    self commands and /me messages

--- util/src/entities/Message.ts
+++ util/src/entities/Message.ts
@@ -39,11 +39,13 @@ export enum MessageType {
 	USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2 = 10,
 	USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3 = 11,
 	CHANNEL_FOLLOW_ADD = 12,
+	ACTION = 13, // /me messages
 	GUILD_DISCOVERY_DISQUALIFIED = 14,
 	GUILD_DISCOVERY_REQUALIFIED = 15,
 	ENCRYPTED = 16,
 	REPLY = 19,
-	APPLICATION_COMMAND = 20,
+	APPLICATION_COMMAND = 20, // application command or self command invocation
+	SELF_COMMAND_SCRIPT = 21, // self command scripts
 	ROUTE_ADDED = 41, // custom message routing: new route affecting that channel
 	ROUTE_DISABLED = 42, // custom message routing: given route no longer affecting that channel
 	ENCRYPTION = 50,
