16c7ec50e8b634f5d11df1ee7bd595ade1805f5b
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-07-02 22:09:16 +1000

    set message max length manually

--- util/src/entities/Message.ts
+++ util/src/entities/Message.ts
@@ -114,7 +114,7 @@ export class Message extends BaseClass {
 	@ManyToOne(() => Application)
 	application?: Application;
 
-	@Column({ nullable: true })
+	@Column({ nullable: true, length: 4000 })
 	content?: string;
 
 	@Column()
