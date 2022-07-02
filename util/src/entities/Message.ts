b95cfbee1919bd4c6364a6c98dbbc27b05b2ff9e
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-07-02 22:59:50 +1000

    type longtext does not support length

--- util/src/entities/Message.ts
+++ util/src/entities/Message.ts
@@ -114,7 +114,7 @@ export class Message extends BaseClass {
 	@ManyToOne(() => Application)
 	application?: Application;
 
-	@Column({ nullable: true, length: "4000", type: "longtext" })
+	@Column({ nullable: true, type: "longtext" })
 	content?: string;
 
 	@Column()
