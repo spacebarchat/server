7af574b0e8711f7522f258a5b995be2f4cd323bd
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-08-07 03:06:52 +0200

    Clean up commented out code in BaseClass

--- util/src/entities/BaseClass.ts
+++ util/src/entities/BaseClass.ts
@@ -8,30 +8,13 @@ export class BaseClassWithoutId extends BaseEntity {
 		this.assign(props);
 	}
 
-	/*private get construct(): any {
-		return this.constructor;
-	}*/
-
-	/*private get metadata() {
-		console.log("getMetadata")
-		return dataSource.getRepository(this.constructor).metadata as EntityMetadata;
-		//return this.construct.getRepository().metadata as EntityMetadata;
-	}*/
-
 	assign(props: any = {}) {
 		//console.log(`assign (${typeof this})...`)
 		delete props.opts;
 		delete props.props;
-
-		/*const properties = new Set(
-			this.metadata.columns
-				.map((x: any) => x.propertyName)
-				.concat(this.metadata.relations.map((x) => x.propertyName))
-		);*/
 		// will not include relational properties
 
 		for (const key in props) {
-			//if (!properties.has(key)) continue;
 			// @ts-ignore
 			const setter = this[`set${key.capitalize()}`]; // use setter function if it exists
 
@@ -43,35 +26,6 @@ export class BaseClassWithoutId extends BaseEntity {
 			}
 		}
 	}
-
-	/*toJSON(): any {
-		console.log("toJSON...")
-		return Object.fromEntries(
-			this.metadata.columns // @ts-ignore
-				.map((x) => [x.propertyName, this[x.propertyName]]) // @ts-ignore
-				.concat(this.metadata.relations.map((x) => [x.propertyName, this[x.propertyName]]))
-		);
-	}
-
-	static increment<T extends BaseClass>(
-		conditions: FindOptionsWhere<T>,
-		propertyPath: string,
-		value: number | string
-	) {
-		console.log("increment...")
-		const repository = this.getRepository();
-		return repository.increment(conditions, propertyPath, value);
-	}
-
-	static decrement<T extends BaseClass>(
-		conditions: FindOptionsWhere<T>,
-		propertyPath: string,
-		value: number | string
-	) {
-		console.log("increment...")
-		const repository = this.getRepository();
-		return repository.decrement(conditions, propertyPath, value);
-	}*/
 }
 
 export const PrimaryIdColumn = process.env.DATABASE?.startsWith("mongodb") ? ObjectIdColumn : PrimaryColumn;
