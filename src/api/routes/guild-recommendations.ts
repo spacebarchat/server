9a094da14dfd809dd25aa9094269f5cc9ee931ba
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-08-08 22:51:01 +0200

    Remove todo line that has been implemented.

--- src/api/routes/guild-recommendations.ts
+++ src/api/routes/guild-recommendations.ts
@@ -9,10 +9,7 @@ const router = Router();
 router.get("/", route({}), async (req: Request, res: Response) => {
 	const { limit, personalization_disabled } = req.query;
 	var showAllGuilds = Config.get().guild.discovery.showAllGuilds;
-	// ! this only works using SQL querys
-	// TODO: implement this with default typeorm query
-	// const guilds = await Guild.find({ where: { features: "DISCOVERABLE" } }); //, take: Math.abs(Number(limit)) });
-
+	
 	const genLoadId = (size: Number) =>
 		[...Array(size)]
 			.map(() => Math.floor(Math.random() * 16).toString(16))
