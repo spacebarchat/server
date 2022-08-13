44c121357ac73ca679d9ef865e80d975ea632b5e
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2022-08-13 02:42:08 +0200

    feat(plugins): event system

--- src/util/plugin/PluginLoader.ts
+++ src/util/plugin/PluginLoader.ts
@@ -6,27 +6,34 @@ const root = process.env.PLUGIN_LOCATION || "../plugins";
 
 let pluginsLoaded = false;
 export class PluginLoader {
-    public static loadPlugins() {
+	public static loadPlugins() {
+		console.log(`Plugin root directory: ${path.resolve(root)}`);
+		const dirs = fs.readdirSync(root).filter((x) => {
+			try {
+				fs.readdirSync(path.join(root, x));
+				return true;
+			} catch (e) {
+				return false;
+			}
+		});
+		console.log(dirs);
+		dirs.forEach(async (x) => {
+			let modPath = path.resolve(path.join(root, x));
+			console.log(`Trying to load plugin: ${modPath}`);
+			const manifest = require(path.join(modPath, "plugin.json")) as PluginManifest;
+			console.log(
+				`Plugin info: ${manifest.name} (${manifest.id}), written by ${manifest.authors}, available at ${manifest.repository}`
+			);
+			const module_ = require(path.join(modPath, "dist", "index.js")) as Plugin;
+			try {
+				await module_.init();
+				module_.emit("loaded");
+			} catch (error) {
+				module_.emit("error", error);
+			}
+		});
 
-        console.log(`Plugin root directory: ${path.resolve(root)}`);
-        const dirs = fs.readdirSync(root).filter(x => {
-            try {
-                fs.readdirSync(path.join(root, x));
-                return true; 
-            } catch (e) { 
-                return false; 
-            }
-        });
-        console.log(dirs)
-        dirs.forEach(x=>{
-            let modPath = path.resolve(path.join(root, x));
-            console.log(`Trying to load plugin: ${modPath}`)
-            const manifest = require(path.join(modPath, "plugin.json")) as PluginManifest;
-            console.log(`Plugin info: ${manifest.name} (${manifest.id}), written by ${manifest.authors}, available at ${manifest.repository}`)
-            const module_ = require(path.join(modPath, "dist", "index.js")) as Plugin;
-            module_.onPluginLoaded();
-        })
-        //
-        //module_.pluginPath = 
-    }
+		//
+		//module_.pluginPath =
+	}
 }
