3f2a5874f1ef3028abe1eca558e432ec3beb5dae
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2022-08-13 02:42:08 +0200

    feat(plugins): event system

--- src/util/plugin/Plugin.ts
+++ src/util/plugin/Plugin.ts
@@ -1,5 +1,13 @@
-export class Plugin {
-    onPluginLoaded() {
-        console.log('no onpluginloaded!')
-    }
+import EventEmitter from "events";
+import { TypedEventEmitter } from "@fosscord/util";
+
+type PluginEvents = {
+	error: (error: Error | unknown) => void;
+	loaded: () => void;
+};
+
+export class Plugin extends (EventEmitter as new () => TypedEventEmitter<PluginEvents>) {
+	async init() {
+		// insert default config into database?
+	}
 }
