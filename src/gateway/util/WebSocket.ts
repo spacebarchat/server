5e86d7ab9c5200d794c3adb2b422d20a2aefd2ce
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2022-08-13 02:00:50 +0200

    restructure to single project

---
+++ src/gateway/util/WebSocket.ts
@@ -0,0 +1,22 @@
+import { Intents, Permissions } from "@fosscord/util";
+import WS from "ws";
+import { Deflate } from "zlib";
+
+export interface WebSocket extends WS {
+	version: number;
+	user_id: string;
+	session_id: string;
+	encoding: "etf" | "json";
+	compress?: "zlib-stream";
+	shard_count?: number;
+	shard_id?: number;
+	deflate?: Deflate;
+	heartbeatTimeout: NodeJS.Timeout;
+	readyTimeout: NodeJS.Timeout;
+	intents: Intents;
+	sequence: number;
+	permissions: Record<string, Permissions>;
+	events: Record<string, Function>;
+	member_events: Record<string, Function>;
+	listen_options: any;
+}
