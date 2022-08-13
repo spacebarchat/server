c03f3623ea6cc90b35769cdb954bd22c5058016e
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2022-08-13 02:14:41 +0200

    feat(plugins): loader

---
+++ src/util/plugin/PluginLoader.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import fs from "fs";
+import { Plugin, PluginManifest } from "./";
+
+const root = process.env.PLUGIN_LOCATION || "../plugins";
+
+let pluginsLoaded = false;
+export class PluginLoader {
+    public static loadPlugins() {
+
+        console.log(`Plugin root directory: ${path.resolve(root)}`);
+        const dirs = fs.readdirSync(root).filter(x => {
+            try {
+                fs.readdirSync(path.join(root, x));
+                return true; 
+            } catch (e) { 
+                return false; 
+            }
+        });
+        console.log(dirs)
+        dirs.forEach(x=>{
+            let modPath = path.resolve(path.join(root, x));
+            console.log(`Trying to load plugin: ${modPath}`)
+            const manifest = require(path.join(modPath, "plugin.json")) as PluginManifest;
+            console.log(`Plugin info: ${manifest.name} (${manifest.id}), written by ${manifest.authors}, available at ${manifest.repository}`)
+            const module_ = require(path.join(modPath, "dist", "index.js")) as Plugin;
+            module_.onPluginLoaded();
+        })
+        //
+        //module_.pluginPath = 
+    }
+}
