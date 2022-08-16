03ba7e40c52445e807ca9d3a76cc8f63a27ff21a
Erkin Alp Güney <erkinalp9035@gmail.com>
2022-08-16 19:17:29 +0300

    Fix the latest SKU errors and further debranding

--- src/api/routes/store/published-listings/skus/#sku_id/subscription-plans.ts
+++ src/api/routes/store/published-listings/skus/#sku_id/subscription-plans.ts
@@ -9,23 +9,23 @@ const skus = new Map([
 		[
 			{
 				id: "511651856145973248",
-				name: "Premium Monthly (Legacy)",
+				name: "Individual Premium Tier 2 Monthly (Legacy)",
 				interval: 1,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "521842865731534868",
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			},
 			{
 				id: "511651860671627264",
-				name: "Premium Yearly (Legacy)",
+				name: "Individiual Premium Tier 2 Yearly (Legacy)",
 				interval: 2,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "521842865731534868",
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			}
@@ -36,23 +36,34 @@ const skus = new Map([
 		[
 			{
 				id: "511651871736201216",
-				name: "Premium Classic Monthly",
+				name: "Individual Premium Tier 1 Monthly",
 				interval: 1,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "521846918637420545",
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			},
 			{
 				id: "511651876987469824",
-				name: "Premium Classic Yearly",
+				name: "Individual Premum Tier 1 Yearly",
 				interval: 2,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "521846918637420545",
-				currency: "usd",
+				currency: "eur",
+				price: 0,
+				price_tier: null
+			},
+			{
+				id: "978380684370378761",
+				name: "Individual Premum Tier 0",
+				interval: 2,
+				interval_count: 1,
+				tax_inclusive: true,
+				sku_id: "521846918637420545",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			}
@@ -63,34 +74,34 @@ const skus = new Map([
 		[
 			{
 				id: "642251038925127690",
-				name: "Premium Quarterly",
+				name: "Individual Premium Tier 2 Quarterly",
 				interval: 1,
 				interval_count: 3,
 				tax_inclusive: true,
 				sku_id: "521847234246082599",
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			},
 			{
 				id: "511651880837840896",
-				name: "Premium Monthly",
+				name: "Individual Premium Tier 2 Monthly",
 				interval: 1,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "521847234246082599",
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			},
 			{
 				id: "511651885459963904",
-				name: "Premium Yearly",
+				name: "Individual Premium Tier 2 Yearly",
 				interval: 2,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "521847234246082599",
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			}
@@ -101,25 +112,25 @@ const skus = new Map([
 		[
 			{
 				id: "590665532894740483",
-				name: "Server Boost Monthly",
+				name: "Crowd Premium Monthly",
 				interval: 1,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "590663762298667008",
 				discount_price: 0,
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			},
 			{
 				id: "590665538238152709",
-				name: "Server Boost Yearly",
+				name: "Crowd Premium Yearly",
 				interval: 2,
 				interval_count: 1,
 				tax_inclusive: true,
 				sku_id: "590663762298667008",
 				discount_price: 0,
-				currency: "usd",
+				currency: "eur",
 				price: 0,
 				price_tier: null
 			}
