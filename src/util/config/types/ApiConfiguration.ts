a0892859d4cd567991579c40b13836c6ddbdc22b
David Sundberg <111130321+DavidStarDev@users.noreply.github.com>
2022-08-18 18:24:04 +0200

    Update ApiConfiguration.ts

--- src/util/config/types/ApiConfiguration.ts
+++ src/util/config/types/ApiConfiguration.ts
@@ -1,5 +1,5 @@
 export class ApiConfiguration {
 	defaultVersion = "9";
 	activeVersions: string[] = ["6", "7", "8", "9"];
-	useFosscordEnhancements: boolean = true;
+	useFosscordEnhancements = true;
 }
