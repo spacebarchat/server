3d91eba74d6841d8f7c77a5f4e6e13e97d2d2b18
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-08-18 05:48:42 +0200

    Update build script, remove ts-transform-paths

--- scripts/build/remap_imports.js
+++ scripts/build/remap_imports.js
@@ -6,17 +6,10 @@ const { execIn, parts, getDirs, walk, sanitizeVarName } = require("../utils");
 
 module.exports = function (config) {
 	console.log(`==> Remapping module imports...`);
-	Object.keys(config.paths).forEach((mod) => {
-		let files = walk(config.distDir).filter((x) => x.endsWith(".js"));
-		files.forEach((x) => {
-			let fc = fs.readFileSync(x).toString();
-			if (fc.includes(mod)) {
-				//find relative path to other directory
-				let relPath = path.relative(x, path.resolve(path.join(config.distDir, config.paths[mod])));
-				if (config.verbose) console.log(`  ==> Remapping import of ${mod} to ${relPath} in ${path.relative(config.distDir, x)}`);
-				fc = fc.replace(mod, relPath);
-				fs.writeFileSync(x, fc);
-			}
-		});
+	let files = walk(config.distDir).filter((x) => x.endsWith(".js"));
+	files.forEach((x) => {
+		let fc = fs.readFileSync(x).toString();
+		fc = fc.replaceAll("@fosscord/", "#");
+		fs.writeFileSync(x, fc);
 	});
 };
