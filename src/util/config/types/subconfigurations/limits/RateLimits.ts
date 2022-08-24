9fc4d4bd5e5aed6b126f52f92ea256caf374dd07
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-08-24 03:01:57 +0200

    case insensitive header for rate limits, fix rate limit default settings
    Also disabled rate limit bypass right as it doesn't work...

--- src/util/config/types/subconfigurations/limits/RateLimits.ts
+++ src/util/config/types/subconfigurations/limits/RateLimits.ts
@@ -1,18 +1,18 @@
 import { RouteRateLimit, RateLimitOptions } from ".";
 
 export class RateLimits {
-    disabled: boolean = true;
-    ip: Omit<RateLimitOptions, "bot_count"> = {
-        count: 500,
-        window: 5
-    };
-    global: RateLimitOptions = {
-        count: 250,
-        window: 5
-    };
-    error: RateLimitOptions = {
-        count: 10,
-        window: 5
-    };
-    routes: RouteRateLimit;
+	disabled: boolean = true;
+	ip: Omit<RateLimitOptions, "bot_count"> = {
+		count: 500,
+		window: 5
+	};
+	global: RateLimitOptions = {
+		count: 250,
+		window: 5
+	};
+	error: RateLimitOptions = {
+		count: 10,
+		window: 5
+	};
+	routes: RouteRateLimit = new RouteRateLimit();
 }
