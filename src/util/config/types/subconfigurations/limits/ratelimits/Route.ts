da2fe81a8b6b757c79eec653e8481c6c34124344
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-08-24 03:01:57 +0200

    case insensitive header for rate limits, fix rate limit default settings
    Also disabled rate limit bypass right as it doesn't work...

--- src/util/config/types/subconfigurations/limits/ratelimits/Route.ts
+++ src/util/config/types/subconfigurations/limits/ratelimits/Route.ts
@@ -2,18 +2,18 @@ import { AuthRateLimit } from ".";
 import { RateLimitOptions } from "./RateLimitOptions";
 
 export class RouteRateLimit {
-    guild: RateLimitOptions = {
-        count: 5,
-        window: 5
-    };
-    webhook: RateLimitOptions = {
-        count: 10,
-        window: 5
-    };
-    channel: RateLimitOptions = {
-        count: 10,
-        window: 5
-    };
-    auth: AuthRateLimit;
-    // TODO: rate limit configuration for all routes
+	guild: RateLimitOptions = {
+		count: 5,
+		window: 5
+	};
+	webhook: RateLimitOptions = {
+		count: 10,
+		window: 5
+	};
+	channel: RateLimitOptions = {
+		count: 10,
+		window: 5
+	};
+	auth: AuthRateLimit = new AuthRateLimit();
+	// TODO: rate limit configuration for all routes
 }
