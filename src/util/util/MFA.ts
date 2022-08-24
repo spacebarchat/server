57583d049aaf77a267976f7cdf0e553a3599929c
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-08-24 03:16:53 +0200

    Configurable MFA backup token length

--- src/util/util/MFA.ts
+++ src/util/util/MFA.ts
@@ -1,9 +1,10 @@
 import crypto from "crypto";
+import { Config } from ".";
 import { BackupCode } from "../entities/BackupCodes";
 
 export function generateMfaBackupCodes(user_id: string) {
 	let backup_codes: BackupCode[] = [];
-	for (let i = 0; i < 10; i++) {
+	for (let i = 0; i < Config.get().security.mfaBackupCodeCount; i++) {
 		const code = BackupCode.create({
 			user: { id: user_id },
 			code: crypto.randomBytes(4).toString("hex"), // 8 characters
