a75d162d5aaee6ba00800733217b75b5ef6f2ca3
Puyodead1 <puyodead@protonmail.com>
2022-08-30 16:50:12 -0400

    thread starter message

--- src/api/routes/channels/#channel_id/messages/#message_id/threads.ts
+++ src/api/routes/channels/#channel_id/messages/#message_id/threads.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { Channel } from "../../../../../../util";
+import { Channel, Message, MessageType, OrmUtils } from "../../../../../../util";
 import { ThreadCreateSchema } from "../../../../../../util/schemas/ThreadCreateSchema";
 import { route } from "../../../../../util";
 
@@ -8,6 +8,13 @@ const router = Router();
 router.post("/", route({ body: "ThreadCreateSchema" }), async (req: Request, res: Response) => {
 	const { channel_id, message_id } = req.params;
 	const body = req.body as ThreadCreateSchema;
+
+	const message = await Message.findOneOrFail({
+		where: {
+			id: message_id,
+			channel_id
+		}
+	});
 
 	const thread = await Channel.createThreadChannel(
 		{
@@ -23,6 +30,27 @@ router.post("/", route({ body: "ThreadCreateSchema" }), async (req: Request, res
 		req.user_id,
 		{ keepId: true }
 	);
+
+	await OrmUtils.mergeDeep(new Message(), {
+		message_reference: {
+			channel_id,
+			guild_id: thread.guild_id,
+			message_id
+		},
+		type: MessageType.THREAD_STARTER_MESSAGE,
+		content: "",
+		author_id: message.author_id,
+		channel_id: thread.id,
+		sticker_items: [],
+		guild_id: thread.guild_id,
+		attachments: [],
+		embeds: [],
+		reactions: []
+	}).save();
+
+	thread.message_count = (thread.message_count || 0) + 1;
+	thread.total_message_sent = (thread.total_message_sent || 0) + 1;
+	await thread.save();
 
 	res.status(201).json(thread);
 });
