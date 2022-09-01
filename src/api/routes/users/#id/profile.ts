36fe585fc518c22f4f2383d420c22c2d5f3526e5
Puyodead1 <puyodead@protonmail.com>
2022-09-01 11:05:38 -0400

    fix user profile not displaying correctly

--- src/api/routes/users/#id/profile.ts
+++ src/api/routes/users/#id/profile.ts
@@ -73,6 +73,12 @@ router.get("/", route({ test: { response: { body: "UserProfileResponse" } } }), 
 		bot: user.bot
 	};
 
+	const userProfile = {
+		bio: req.user_bot ? null : user.bio,
+		accent_color: user.accent_color,
+		banner: user.banner
+	};
+
 	const guildMemberDto = guild_member
 		? {
 				avatar: guild_member.avatar,
@@ -104,8 +110,9 @@ router.get("/", route({ test: { response: { body: "UserProfileResponse" } } }), 
 		premium_since: user.premium_since, // TODO
 		mutual_guilds: mutual_guilds, // TODO {id: "", nick: null} when ?with_mutual_guilds=true
 		user: userDto,
-		guild_member: guildMemberDto,
-		guild_member_profile: guildMemberProfile
+		user_profile: userProfile,
+		guild_member: guild_id && guildMemberDto,
+		guild_member_profile: guild_id && guildMemberProfile
 	});
 });
 
