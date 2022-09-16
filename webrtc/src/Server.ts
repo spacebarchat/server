27792331178cb93acc4b065c02901e01c6fc60eb
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-09-16 13:51:32 +1000

    hmm

--- webrtc/src/Server.ts
+++ webrtc/src/Server.ts
@@ -22,18 +22,18 @@ export class Server {
 			});
 		}
 
-		this.server.on("upgrade", (request, socket, head) => {
-			if (!request.url?.includes("voice")) return;
-			this.ws.handleUpgrade(request, socket, head, (socket) => {
-				// @ts-ignore
-				socket.server = this;
-				this.ws.emit("connection", socket, request);
-			});
-		});
+		// this.server.on("upgrade", (request, socket, head) => {
+		// 	if (!request.url?.includes("voice")) return;
+		// 	this.ws.handleUpgrade(request, socket, head, (socket) => {
+		// 		// @ts-ignore
+		// 		socket.server = this;
+		// 		this.ws.emit("connection", socket, request);
+		// 	});
+		// });
 
 		this.ws = new ws.Server({
 			maxPayload: 1024 * 1024 * 100,
-			noServer: true
+			server: this.server,
 		});
 		this.ws.on("connection", Connection);
 		this.ws.on("error", console.error);
