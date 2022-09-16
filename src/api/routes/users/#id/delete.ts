87790c208be8218bd7e0127cba41328d72ecc270
Erkin Alp Güney <erkinalp9035@gmail.com>
2022-09-16 16:05:19 +0300

    fixed up the schema

--- src/api/routes/users/#id/delete.ts
+++ src/api/routes/users/#id/delete.ts
@@ -4,17 +4,17 @@ import { Request, Response, Router } from "express";
 
 const router = Router();
 
-router.post("/", route({ body: "UserDeleteSchema", right: "MANAGE_USERS" }), async (req: Request, res: Response) => {
+router.post("/", route({ right: "MANAGE_USERS" }), async (req: Request, res: Response) => {
 	const body = req.body as UserDeleteSchema;
 
-	let user = await User.findOneOrFail({ where: { id: req.params.user_id }, select: [...PrivateUserProjection, "data"] });
-	await Promise.all([Member.delete({ id: req.params.user_id }), User.delete({ id: req.params.user_id })]);
+	let user = await User.findOneOrFail({ where: { id: req.params.id }, select: [...PrivateUserProjection, "data"] });
+	await Promise.all([Member.delete({ id: req.params.id }), User.delete({ id: req.params.id })]);
 
 	// TODO: respect intents as USER_DELETE has potential to cause privacy issues
 	await emitEvent({
 		event: "USER_DELETE",
 		user_id: req.user_id,
-		data: { user_id: req.params.user_id }
+		data: { user_id: req.params.id }
 	} as UserDeleteEvent);
 
 	res.sendStatus(204);
