4cdb547f7608f6832825113fc1f77690e7f5960c
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-09-26 13:04:55 +1000

    Bring back loginRedirect but fix it. Also move some assets out of public

---
+++ assets/preload-plugins/loginRedirect.js
@@ -0,0 +1,17 @@
+if (window.location.hostname == "127.0.0.1" || window.location.hostname == "localhost")
+	throw "disabling loginRedirect because localhost";
+
+const redirectIfOnLogin = () => {
+	const path = window.location.pathname;
+	if (path == "/login" || path == "/register" || !localStorage.getItem("token")) {
+		window.location.pathname = "/login";
+		window.location.reload();
+	}
+};
+
+const observer = new MutationObserver((mutations) => {
+	redirectIfOnLogin();
+});
+observer.observe(document, { subtree: true, childList: true });
+
+redirectIfOnLogin();
