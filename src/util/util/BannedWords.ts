252bc880e8d7735674b657511500f167a1f2d5b6
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-10-25 11:20:01 +1100

    Remove whitespace characters in banned words detection

--- src/util/util/BannedWords.ts
+++ src/util/util/BannedWords.ts
@@ -1,5 +1,6 @@
 import fs from "fs/promises";
 import path from "path";
+import { InvisibleCharacters } from "./InvisibleCharacters";
 
 var words: string[];
 
@@ -20,6 +21,7 @@ export const BannedWords = {
 	get: () => words,
 
 	find: (val: string) => {
+		InvisibleCharacters.forEach(x => val = val.replaceAll(x, ""));
 		return words.some((x) => val.indexOf(x) != -1);
 	},
 };
