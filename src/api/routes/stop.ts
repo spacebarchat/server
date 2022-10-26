5f0d16d4bfc5efed7f73913241a28101391b04b2
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-10-26 19:50:51 +1100

    Make /stop route not ugly as hell

--- src/api/routes/stop.ts
+++ src/api/routes/stop.ts
@@ -1,28 +1,12 @@
 import { Router, Request, Response } from "express";
 import { route } from "@fosscord/api";
-import { User } from "@fosscord/util";
 
 const router: Router = Router();
 
-router.post("/", route({}), async (req: Request, res: Response) => {
-	//EXPERIMENTAL: have an "OPERATOR" platform permission implemented for this API route
-	const user = await User.findOneOrFail({
-		where: { id: req.user_id },
-		select: ["rights"],
-	});
-	if ((Number(user.rights) << Number(0)) % Number(2) == Number(1)) {
-		console.log("user that POSTed to the API was ALLOWED");
-		console.log(user.rights);
-		res.sendStatus(200);
-		process.kill(process.pid, "SIGTERM");
-	} else {
-		console.log("operation failed");
-		console.log(user.rights);
-		res.sendStatus(403);
-	}
+router.post("/", route({ right: "OPERATOR" }), async (req: Request, res: Response) => {
+	console.log(`/stop was called by ${req.user_id} at ${new Date()}`);
+	res.sendStatus(200);
+	process.kill(process.pid, "SIGTERM");
 });
 
 export default router;
-
-//THIS API CAN ONLY BE USED BY USERS WITH THE 'OPERATOR' RIGHT (which is the value of 1) ONLY IF ANY OTHER RIGHTS ARE ADDED OR IF THE USER DOESNT HAVE PERMISSION,
-//THE REQUEST WILL RETURN 403 'FORBIDDEN'
