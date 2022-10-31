152ed6da9568982ba1a3a9db5cd6dfa9e1ace4df
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-10-31 12:18:30 +1100

    Add token check back to loginRedirect

--- assets/preload-plugins/loginRedirect.js
+++ assets/preload-plugins/loginRedirect.js
@@ -1,7 +1,7 @@
 if (window.location.hostname !== "127.0.0.1" && window.location.hostname !== "localhost") {
 	const redirectIfOnLogin = () => {
 		const path = window.location.pathname;
-		if (path == "/login" || path == "/register" /* || !window.localStorage.getItem("token") */) {
+		if (path == "/login" || path == "/register" || !window.localStorage.getItem("token")) {
 			window.location.pathname = "/login";
 			//window.location.reload();
 		}
