30e65415e431e396c0092d8efad3aa82aa0a8307
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-12-21 15:37:16 +1100

    Set default cdn endpoints so that local testing works

--- src/util/config/types/CdnConfiguration.ts
+++ src/util/config/types/CdnConfiguration.ts
@@ -4,4 +4,7 @@ export class CdnConfiguration extends EndpointConfiguration {
 	resizeHeightMax: number = 1000;
 	resizeWidthMax: number = 1000;
 	imagorServerUrl: string | null = null;
+
+	endpointPublic: string | null = "http://localhost:3001";
+	endpointPrivate: string | null = "http://localhost:3001";
 }
