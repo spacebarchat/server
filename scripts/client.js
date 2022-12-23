6a00d6029f76eda5209cf814d91c8e0c5987f1f8
Puyodead1 <puyodead@proton.me>
2022-12-23 03:18:02 -0500

    Patch client to use Discord connection

--- scripts/client.js
+++ scripts/client.js
@@ -18,15 +18,16 @@
 
 const path = require("path");
 const fetch = require("node-fetch");
-const http = require('http');
-const https = require('https');
+const http = require("http");
+const https = require("https");
 const fs = require("fs/promises");
 const { existsSync } = require("fs");
 
 // https://stackoverflow.com/a/62500224
 const httpAgent = new http.Agent({ keepAlive: true });
 const httpsAgent = new https.Agent({ keepAlive: true });
-const agent = (_parsedURL) => _parsedURL.protocol == 'http:' ? httpAgent : httpsAgent;
+const agent = (_parsedURL) =>
+	_parsedURL.protocol == "http:" ? httpAgent : httpsAgent;
 
 const CACHE_PATH = path.join(__dirname, "..", "assets", "cache");
 const BASE_URL = "https://discord.com";
@@ -40,6 +41,10 @@ const INDEX_SCRIPTS = [
 	"f98a039261c37f892cbf", // 0?
 	"4470c87bb13810847db0", // ~4500.
 ];
+
+const splice = (a, b, index) => {
+	return a.substring(0, index) + b + a.substring(index);
+};
 
 const doPatch = (content) => {
 	//remove nitro references
@@ -55,7 +60,10 @@ const doPatch = (content) => {
 	content = content.replaceAll(/ Discord /g, ` ${INSTANCE_NAME} `);
 	content = content.replaceAll(/Discord /g, `${INSTANCE_NAME} `);
 	content = content.replaceAll(/ Discord/g, ` ${INSTANCE_NAME}`);
-	content = content.replaceAll(/Discord Premium/g, `${INSTANCE_NAME} Premium`);
+	content = content.replaceAll(
+		/Discord Premium/g,
+		`${INSTANCE_NAME} Premium`,
+	);
 	content = content.replaceAll(/Discord Nitro/g, `${INSTANCE_NAME} Premium`);
 	content = content.replaceAll(/Discord's/g, `${INSTANCE_NAME}'s`);
 	//content = content.replaceAll(/DiscordTag/g, "FosscordTag");
@@ -66,41 +74,49 @@ const doPatch = (content) => {
 		['"Server"', '"Guild"'],
 		['"Server ', '"Guild '],
 		[' Server"', ' Guild"'],
-		[' Server ', ' Guild '],
+		[" Server ", " Guild "],
 
 		['"Server."', '"Guild."'],
 		[' Server."', ' Guild."'],
 
 		['"Server."', '"Guild,"'],
 		[' Server,"', ' Guild,"'],
-		[' Server,', ' Guild,'],
+		[" Server,", " Guild,"],
 
 		['"Servers"', '"Guilds"'],
 		['"Servers ', '"Guilds '],
 		[' Servers"', ' Guilds"'],
-		[' Servers ', ' Guilds '],
+		[" Servers ", " Guilds "],
 
 		['"Servers."', '"Guilds."'],
 		[' Servers."', ' Guilds,"'],
 
 		['"Servers,"', '"Guilds,"'],
 		[' Servers,"', ' Guilds,"'],
-		[' Servers,', ' Guilds,'],
+		[" Servers,", " Guilds,"],
 
-		['\nServers', '\nGuilds'],
+		["\nServers", "\nGuilds"],
 	];
-	serverVariations.forEach(x => serverVariations.push([x[0].toLowerCase(), x[1].toLowerCase()]));
-	serverVariations.forEach(x => content = content.replaceAll(x[0], x[1]));
+	serverVariations.forEach((x) =>
+		serverVariations.push([x[0].toLowerCase(), x[1].toLowerCase()]),
+	);
+	serverVariations.forEach((x) => (content = content.replaceAll(x[0], x[1])));
 
 	// sentry
-	content = content.replaceAll("https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984", "https://05e8e3d005f34b7d97e920ae5870a5e5@sentry.thearcanebrony.net/6");
+	content = content.replaceAll(
+		"https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
+		"https://05e8e3d005f34b7d97e920ae5870a5e5@sentry.thearcanebrony.net/6",
+	);
 
 	//logos
 	content = content.replaceAll(
 		"M23.0212 1.67671C21.3107 0.879656 19.5079 0.318797 17.6584 0C17.4062 0.461742 17.1749 0.934541 16.9708 1.4184C15.003 1.12145 12.9974 1.12145 11.0283 1.4184C10.819 0.934541 10.589 0.461744 10.3368 0.00546311C8.48074 0.324393 6.67795 0.885118 4.96746 1.68231C1.56727 6.77853 0.649666 11.7538 1.11108 16.652C3.10102 18.1418 5.3262 19.2743 7.69177 20C8.22338 19.2743 8.69519 18.4993 9.09812 17.691C8.32996 17.3997 7.58522 17.0424 6.87684 16.6135C7.06531 16.4762 7.24726 16.3387 7.42403 16.1847C11.5911 18.1749 16.408 18.1749 20.5763 16.1847C20.7531 16.3332 20.9351 16.4762 21.1171 16.6135C20.41 17.0369 19.6639 17.3997 18.897 17.691C19.3052 18.4993 19.7718 19.2689 20.3021 19.9945C22.6677 19.2689 24.8929 18.1364 26.8828 16.6466H26.8893C27.43 10.9731 25.9665 6.04728 23.0212 1.67671ZM9.68041 13.6383C8.39754 13.6383 7.34085 12.4453 7.34085 10.994C7.34085 9.54272 8.37155 8.34973 9.68041 8.34973C10.9893 8.34973 12.0395 9.54272 12.0187 10.994C12.0187 12.4453 10.9828 13.6383 9.68041 13.6383ZM18.3161 13.6383C17.0332 13.6383 15.9765 12.4453 15.9765 10.994C15.9765 9.54272 17.0124 8.34973 18.3161 8.34973C19.6184 8.34973 20.6751 9.54272 20.6543 10.994C20.6543 12.4453 19.6184 13.6383 18.3161 13.6383Z",
-		"M 0,0 47.999993,2.7036528e-4 C 48.001796,3.3028172 47.663993,6.5968018 46.991821,9.8301938 43.116101,28.454191 28.452575,43.116441 9.8293509,46.992163 6.5960834,47.664163 3.3023222,48.001868 0,47.999992 Z m 9.8293509,28.735114 v 9.248482 C 22.673599,33.047696 32.857154,22.749268 37.63852,9.829938 H 9.8293509 v 8.679899 H 22.931288 c -3.554489,3.93617 -7.735383,7.257633 -12.373436,9.829938 -0.241031,0.133684 -0.483864,0.265492 -0.7285011,0.395339 z"
+		"M 0,0 47.999993,2.7036528e-4 C 48.001796,3.3028172 47.663993,6.5968018 46.991821,9.8301938 43.116101,28.454191 28.452575,43.116441 9.8293509,46.992163 6.5960834,47.664163 3.3023222,48.001868 0,47.999992 Z m 9.8293509,28.735114 v 9.248482 C 22.673599,33.047696 32.857154,22.749268 37.63852,9.829938 H 9.8293509 v 8.679899 H 22.931288 c -3.554489,3.93617 -7.735383,7.257633 -12.373436,9.829938 -0.241031,0.133684 -0.483864,0.265492 -0.7285011,0.395339 z",
 	);
-	content = content.replaceAll('width:n,height:c,viewBox:"0 0 28 20"', 'width:50,height:50,viewBox:"0 0 50 50"');
+	content = content.replaceAll(
+		'width:n,height:c,viewBox:"0 0 28 20"',
+		'width:50,height:50,viewBox:"0 0 50 50"',
+	);
 
 	// app download links
 	// content = content.replaceAll(
@@ -126,28 +142,126 @@ const doPatch = (content) => {
 	// Stop client from deleting `localStorage` global. Makes `plugins` and `preload-plugins` less annoying.
 	content = content.replaceAll(
 		"delete window.localStorage",
-		"console.log('Prevented deletion of localStorage')"
+		"console.log('Prevented deletion of localStorage')",
 	);
 
 	// fast identify
 	content = content.replaceAll(
 		"e.isFastConnect=t;t?e._doFastConnectIdentify():e._doResumeOrIdentify()",
-		"e.isFastConnect=t; if (t !== undefined) e._doResumeOrIdentify();"
+		"e.isFastConnect=t; if (t !== undefined) e._doResumeOrIdentify();",
 	);
 
 	// disable qr code login
-	content = content.replaceAll(/\w\?\(\d,\w\.jsx\)\(\w*\,{authTokenCallback:this\.handleAuthToken}\):null/g, "null");
+	content = content.replaceAll(
+		/\w\?\(\d,\w\.jsx\)\(\w*\,{authTokenCallback:this\.handleAuthToken}\):null/g,
+		"null",
+	);
+
+	// add logo for discord connection
+	content = content.replaceAll(
+		',941009:(e,t,n)=>{e.exports=n.p+"a13305254c45311c90333469714eedca.png"}',
+		',941009:(e,t,n)=>{e.exports=n.p+"a13305254c45311c90333469714eedca.png"},9410010:(e,t,n)=>{e.exports=n.p+"847541504914fd33810e70a0ea73177e.png"}',
+	);
+
+	// add the discord connection object
+	const CONNECTION_LIST_REGEX = /\[{type:\w*\.\w*\.\w*.*,enabled:\!0\}\]/gs;
+	let connectionList = content.match(CONNECTION_LIST_REGEX);
+	if (connectionList) {
+		connectionList = connectionList[0];
+		let lolConnection = connectionList.match(
+			/,{type:.*LEAGUE_OF_LEGENDS.*,enabled:!0}/g,
+		)[0];
+
+		lolConnection = lolConnection.replaceAll(
+			"LEAGUE_OF_LEGENDS",
+			"DISCORD",
+		);
+		lolConnection = lolConnection.replaceAll(
+			"League of Legends",
+			"Discord",
+		);
+		lolConnection = lolConnection.replaceAll(".LOL", ".DISCORD");
+		const iconMatches = lolConnection.matchAll(/\w\((\d*)\)/g);
+		for (const m of iconMatches) {
+			const fn = m[0];
+			const id = m[1];
+			const newFn = fn.replaceAll(id, "9410010");
+			lolConnection = lolConnection.replaceAll(fn, newFn);
+		}
+		// append to the end of the connection list
+		connectionList =
+			connectionList.substring(0, connectionList.length - 1) +
+			lolConnection +
+			connectionList.substring(connectionList.length - 1);
+
+		// now replace the connection list with the new one
+		content = content.replaceAll(CONNECTION_LIST_REGEX, connectionList);
+	}
+
+	// add the connection color
+	let colorMatch = content.match(
+		/,\w\(\w,\w\.\w\.CRUNCHYROLL,"var\(--crunchyroll\)"\)/g,
+	);
+	if (colorMatch) {
+		colorMatch = colorMatch[0];
+		const colorInjectionIndex =
+			content.indexOf(colorMatch) + colorMatch.length;
+		colorMatch = colorMatch.replaceAll("CRUNCHYROLL", "DISCORD");
+		colorMatch = colorMatch.replaceAll("crunchyroll", "discord");
+		content = splice(content, colorMatch, colorInjectionIndex);
+	}
+
+	// add the connection name
+	let nameMatch = content.match(/;\w\.CRUNCHYROLL="crunchyroll"/gs);
+	if (nameMatch) {
+		nameMatch = nameMatch[0];
+		const nameInjectionIndex =
+			content.indexOf(nameMatch) + nameMatch.length;
+		nameMatch = nameMatch.replaceAll("CRUNCHYROLL", "DISCORD");
+		nameMatch = nameMatch.replaceAll("crunchyroll", "discord");
+		content = splice(content, nameMatch, nameInjectionIndex);
+	}
+
+	// add connection color 2
+	let colorMatch2 = content.match(
+		/(?<=}}\)),\w\(\w,\w\.CRUNCHYROLL.*}}\)(?=,\w\()/g,
+	);
+	if (colorMatch2) {
+		colorMatch2 = colorMatch2[0];
+		const colorInjectionIndex2 =
+			content.indexOf(colorMatch2) + colorMatch2.length;
+		colorMatch2 = colorMatch2.replaceAll("CRUNCHYROLL", "DISCORD");
+		const innerContent = colorMatch2.match(/{.*}/g)[0];
+		const newInnerContent = '{hex:"#5865f2",hsl:{h:235,s:.85,l:.65,a:1}}';
+		colorMatch2 = colorMatch2.replaceAll(innerContent, newInnerContent);
+		content = splice(content, colorMatch2, colorInjectionIndex2);
+	}
+
+	// connection color 3
+	content = content.replaceAll(
+		',CRUNCHYROLL:"#f78b24"',
+		',CRUNCHYROLL:"#f78b24",DISCORD:"#5865f2"',
+	);
+
+	// connection color 4
+	content = content.replaceAll(
+		',CRUNCHYROLL:"hsl(29, calc(var(--saturation-factor, 1) * 93%), 55.5%)"',
+		',CRUNCHYROLL:"hsl(29, calc(var(--saturation-factor, 1) * 93%), 55.5%)",DISCORD:"hsl(235, calc(var(--saturation-factor, 1) * 85%), 65%)"',
+	);
 
 	return content;
 };
 
 const processFile = async (name) => {
-	const res = await fetch(`${BASE_URL}/assets/${name}${name.includes(".") ? "" : ".js"}`, {
-		agent,
-	});
+	const res = await fetch(
+		`${BASE_URL}/assets/${name}${name.includes(".") ? "" : ".js"}`,
+		{
+			agent,
+		},
+	);
 	if (res.status !== 200) {
 		return [];
-	};
+	}
 
 	if (name.includes(".") && !name.includes(".js") && !name.includes(".css")) {
 		await fs.writeFile(path.join(CACHE_PATH, name), await res.buffer());
@@ -158,9 +272,14 @@ const processFile = async (name) => {
 
 	text = doPatch(text);
 
-	await fs.writeFile(path.join(CACHE_PATH, `${name}${name.includes(".") ? "" : ".js"}`), text);
+	await fs.writeFile(
+		path.join(CACHE_PATH, `${name}${name.includes(".") ? "" : ".js"}`),
+		text,
+	);
 
-	return [...new Set(text.match(/\"[A-Fa-f0-9]{20}\"/g))].map(x => x.replaceAll("\"", ""));
+	return [...new Set(text.match(/\"[A-Fa-f0-9]{20}\"/g))].map((x) =>
+		x.replaceAll('"', ""),
+	);
 };
 
 (async () => {
@@ -187,7 +306,13 @@ const processFile = async (name) => {
 
 	process.stdout.moveCursor(0, 1);
 
-	const CACHE_MISSES = (await fs.readFile(path.join(CACHE_PATH, "..", "cacheMisses"))).toString().split("\r").join("").split("\n");
+	const CACHE_MISSES = (
+		await fs.readFile(path.join(CACHE_PATH, "..", "cacheMisses"))
+	)
+		.toString()
+		.split("\r")
+		.join("")
+		.split("\n");
 	while (CACHE_MISSES.length > 0) {
 		const asset = CACHE_MISSES.shift();
 		process.stdout.clearLine(0);
@@ -206,23 +331,27 @@ const processFile = async (name) => {
 
 	process.stdout.moveCursor(0, 1);
 
-	var existing = await fs.readdir(CACHE_PATH);
-	while (existing.length > 0) {
-		var file = existing.shift();
+	// var existing = await fs.readdir(CACHE_PATH);
+	// while (existing.length > 0) {
+	// 	var file = existing.shift();
 
-		process.stdout.clearLine(0);
-		process.stdout.cursorTo(0);
-		process.stdout.write(
-			`Patching existing ${file}. Remaining: ${existing.length}.`,
-		);
+	// 	process.stdout.clearLine(0);
+	// 	process.stdout.cursorTo(0);
+	// 	process.stdout.write(
+	// 		`Patching existing ${file}. Remaining: ${existing.length}.`,
+	// 	);
 
-		var text = (await fs.readFile(path.join(CACHE_PATH, file)));
-		if (file.includes(".js") || file.includes(".css")) {
-			text = doPatch(text.toString());
-			await fs.writeFile(path.join(CACHE_PATH, file), text.toString());
-			assets.push(...[...new Set(text.match(/\"[A-Fa-f0-9]{20}\"/g))].map(x => x.replaceAll("\"", "")));
-		}
-	}
+	// 	var text = await fs.readFile(path.join(CACHE_PATH, file));
+	// 	if (file.includes(".js") || file.includes(".css")) {
+	// 		text = doPatch(text.toString());
+	// 		await fs.writeFile(path.join(CACHE_PATH, file), text.toString());
+	// 		assets.push(
+	// 			...[...new Set(text.match(/\"[A-Fa-f0-9]{20}\"/g))].map((x) =>
+	// 				x.replaceAll('"', ""),
+	// 			),
+	// 		);
+	// 	}
+	// }
 
 	process.stdout.moveCursor(0, 1);
 
@@ -249,9 +378,9 @@ const processFile = async (name) => {
 		process.stdout.cursorTo(0);
 		process.stdout.write(
 			`Caching asset ${asset}. ` +
-			`${i}/${assets.length - 1} = ${Math.floor(
-				(i / (assets.length - 1)) * 100,
-			)}% `
+				`${i}/${assets.length - 1} = ${Math.floor(
+					(i / (assets.length - 1)) * 100,
+				)}% `,
 			// + `Finish at: ${new Date(
 			// 	Date.now() + finishTime,
 			// ).toLocaleTimeString()}`,
