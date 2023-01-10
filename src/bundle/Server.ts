2f065e6b3793f2c2d4ab196a341cf23efb721011
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-01-10 13:19:05 +1100

    Move sentry init to util. use sentry in each module, not just bundle

--- src/bundle/Server.ts
+++ src/bundle/Server.ts
@@ -7,10 +7,7 @@ import * as Gateway from "@fosscord/gateway";
 import { CDNServer } from "@fosscord/cdn";
 import express from "express";
 import { green, bold, yellow } from "picocolors";
-import { Config, initDatabase } from "@fosscord/util";
-import * as Sentry from "@sentry/node";
-import * as Tracing from "@sentry/tracing";
-import * as Integrations from "@sentry/integrations";
+import { Config, initDatabase, Sentry } from "@fosscord/util";
 
 const app = express();
 const server = http.createServer();
@@ -30,81 +27,12 @@ process.on("SIGTERM", async () => {
 async function main() {
 	await initDatabase();
 	await Config.init();
-
-	//Sentry
-	if (Config.get().sentry.enabled) {
-		console.log(
-			`[Bundle] ${yellow(
-				"You are using Sentry! This may slightly impact performance on large loads!",
-			)}`,
-		);
-		Sentry.init({
-			dsn: Config.get().sentry.endpoint,
-			integrations: [
-				new Sentry.Integrations.Http({ tracing: true }),
-				new Tracing.Integrations.Express({ app }),
-				new Tracing.Integrations.Mysql(),
-				new Integrations.RewriteFrames({
-					root: __dirname,
-				}),
-			],
-			tracesSampleRate: Config.get().sentry.traceSampleRate,
-			environment: Config.get().sentry.environment,
-		});
-
-		Sentry.addGlobalEventProcessor((event, hint) => {
-			if (event.transaction) {
-				// Rewrite things that look like IDs to `:id` for sentry
-				event.transaction = event.transaction
-					.split("/")
-					.map((x) => (!parseInt(x) ? x : ":id"))
-					.join("/");
-			}
-
-			// TODO: does this even do anything?
-			delete event.request?.cookies;
-			if (event.request?.headers) {
-				delete event.request.headers["X-Real-Ip"];
-				delete event.request.headers["X-Forwarded-For"];
-				delete event.request.headers["X-Forwarded-Host"];
-				delete event.request.headers["X-Super-Properties"];
-			}
-
-			if (event.breadcrumbs) {
-				event.breadcrumbs = event.breadcrumbs.filter((x) => {
-					// Filter breadcrumbs that we don't care about
-					if (x.message?.includes("identified as")) return false;
-					if (x.message?.includes("[WebSocket] closed")) return false;
-					if (
-						x.message?.includes(
-							"Got Resume -> cancel not implemented",
-						)
-					)
-						return false;
-					if (x.message?.includes("[Gateway] New connection from"))
-						return false;
-
-					return true;
-				});
-			}
-
-			return event;
-		});
-
-		app.use(Sentry.Handlers.requestHandler());
-		app.use(Sentry.Handlers.tracingHandler());
-	}
+	await Sentry.init(app);
 
 	server.listen(port);
 	await Promise.all([api.start(), cdn.start(), gateway.start()]);
 
-	if (Config.get().sentry.enabled) {
-		app.use(Sentry.Handlers.errorHandler());
-		app.use(function onError(err: any, req: any, res: any, next: any) {
-			res.statusCode = 500;
-			res.end(res.sentry + "\n");
-		});
-	}
+	Sentry.errorHandler(app);
 
 	console.log(`[Server] ${green(`listening on port ${bold(port)}`)}`);
 }
---
+++ src/util/util/Sentry.ts
@@ -0,0 +1,102 @@
+import { Config } from "./Config";
+import { yellow } from "picocolors";
+
+import express from "express";
+import * as SentryNode from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+import * as Integrations from "@sentry/integrations";
+
+// Work around for when bundle calls api/etc
+let errorHandlersUsed = false;
+
+export const Sentry = {
+	/** Call BEFORE registering your routes */
+	init: async (app?: express.Application) => {
+		const { enabled, endpoint, traceSampleRate, environment } =
+			Config.get().sentry;
+		if (!enabled) return;
+
+		if (!!SentryNode.getCurrentHub().getClient()) return; // we've already initialised sentry
+
+		console.log("[Sentry] Enabling sentry...");
+
+		if (traceSampleRate >= 0.8) {
+			console.log(
+				`[Sentry] ${yellow(
+					"Your sentry trace sampling rate is >= 80%. For large loads, this may degrade performance.",
+				)}`,
+			);
+		}
+
+		SentryNode.init({
+			dsn: endpoint,
+			integrations: [
+				new SentryNode.Integrations.Http({ tracing: true }),
+				new Tracing.Integrations.Express({ app }),
+				new Tracing.Integrations.Mysql(),
+				new Integrations.RewriteFrames({
+					root: __dirname,
+				}),
+			],
+			tracesSampleRate: traceSampleRate, // naming?
+			environment,
+		});
+
+		SentryNode.addGlobalEventProcessor((event, hint) => {
+			if (event.transaction) {
+				// Rewrite things that look like IDs to `:id` for sentry
+				event.transaction = event.transaction
+					.split("/")
+					.map((x) => (!parseInt(x) ? x : ":id"))
+					.join("/");
+			}
+
+			// TODO: does this even do anything?
+			delete event.request?.cookies;
+			if (event.request?.headers) {
+				delete event.request.headers["X-Real-Ip"];
+				delete event.request.headers["X-Forwarded-For"];
+				delete event.request.headers["X-Forwarded-Host"];
+				delete event.request.headers["X-Super-Properties"];
+			}
+
+			if (event.breadcrumbs) {
+				event.breadcrumbs = event.breadcrumbs.filter((x) => {
+					// Filter breadcrumbs that we don't care about
+					if (x.message?.includes("identified as")) return false;
+					if (x.message?.includes("[WebSocket] closed")) return false;
+					if (
+						x.message?.includes(
+							"Got Resume -> cancel not implemented",
+						)
+					)
+						return false;
+					if (x.message?.includes("[Gateway] New connection from"))
+						return false;
+
+					return true;
+				});
+			}
+
+			return event;
+		});
+
+		if (app) {
+			app.use(SentryNode.Handlers.requestHandler());
+			app.use(SentryNode.Handlers.tracingHandler());
+		}
+	},
+
+	/** Call AFTER registering your routes */
+	errorHandler: (app: express.Application) => {
+		if (!Config.get().sentry.enabled) return;
+		if (errorHandlersUsed) return;
+		errorHandlersUsed = true;
+
+		app.use(SentryNode.Handlers.errorHandler());
+		app.use(function onError(err: any, req: any, res: any, next: any) {
+			res.statusCode = 500;
+			res.end(res.sentry + "\n");
+		});
+	},
+};
