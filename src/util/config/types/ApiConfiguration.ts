754d8dbf64fc242edca3696bf5627e83c150f2bb
TheArcaneBrony <myrainbowdash949@gmail.com>
2023-01-14 19:55:19 +0100

    Add basic info in ping
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- src/util/config/types/ApiConfiguration.ts
+++ src/util/config/types/ApiConfiguration.ts
@@ -2,4 +2,5 @@ export class ApiConfiguration {
 	defaultVersion: string = "9";
 	activeVersions: string[] = ["6", "7", "8", "9"];
 	useFosscordEnhancements: boolean = true;
+	endpointPublic: string = "/api";
 }
