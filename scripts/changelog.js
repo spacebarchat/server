6355b9ffa9fc0a9939e7d4fef7e29c8cdc943e3a
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-01-16 23:26:20 +1100

    Add license preamble to each file.

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,4 +1,22 @@
 /*
+	Fosscord: A FOSS re-implementation and extension of the Discord.com backend.
+	Copyright (C) 2023 Fosscord and Fosscord Contributors
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+	
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
 	Changelogs are baked inside the discord.com web client.
 	To change them, we simply need to update the changelog in a specific file of the client.
 	For v134842, thats 9c4b2d313c6e1c864e89.js, but it'll be different for every version.
