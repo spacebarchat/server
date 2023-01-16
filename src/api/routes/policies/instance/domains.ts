d857af980c83800f1b083d0ce463ad0221ada690
Chris Chrome <christophercookman@gmail.com>
2023-01-15 19:46:41 -0700

    Move endpoints from ping -> policies/instance/domains (#933)

--- src/api/routes/policies/instance/domains.ts
+++ src/api/routes/policies/instance/domains.ts
@@ -5,7 +5,7 @@ import { config } from "dotenv";
 const router = Router();
 
 router.get("/", route({}), async (req: Request, res: Response) => {
-	const { cdn, gateway } = Config.get();
+	const { cdn, gateway, api } = Config.get();
 
 	const IdentityForm = {
 		cdn: cdn.endpointPublic || process.env.CDN || "http://localhost:3001",
@@ -13,6 +13,8 @@ router.get("/", route({}), async (req: Request, res: Response) => {
 			gateway.endpointPublic ||
 			process.env.GATEWAY ||
 			"ws://localhost:3002",
+		defaultApiVersion: api.defaultVersion ?? 9,
+		apiEndpoint: api.endpointPublic ?? "/api",
 	};
 
 	res.json(IdentityForm);
