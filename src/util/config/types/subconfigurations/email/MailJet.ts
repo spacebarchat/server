bf55ebc81fa8d3cc4aa4e6fd3735ff0ee659505a
Puyodead1 <puyodead@proton.me>
2023-01-21 13:53:26 -0500

    Add mailjet transport

--- src/util/config/types/subconfigurations/email/MailJet.ts
+++ src/util/config/types/subconfigurations/email/MailJet.ts
@@ -16,6 +16,7 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-export * from "./MailGun";
-export * from "./MailJet";
-export * from "./SMTP";
+export class MailJetConfiguration {
+	apiKey: string | null = null;
+	apiSecret: string | null = null;
+}
