4c82fd1bb4c3bc140987f49aeb288ec1dc072c39
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-02-03 14:40:41 +1100

    run add:license

--- src/api/routes/read-states/ack-bulk.ts
+++ src/api/routes/read-states/ack-bulk.ts
@@ -1,3 +1,21 @@
+/*
+	Fosscord: A FOSS re-implementation and extension of the Discord.com backend.
+	Copyright (C) 2023 Fosscord and Fosscord Contributors
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+	
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { Router, Request, Response } from "express";
 import { route } from "@fosscord/api";
 import { AckBulkSchema, ReadState } from "@fosscord/util";
