8503135a166235b35a8c0203ce40ca24e18303bf
Casraw <casraw70@googlemail.com>
2023-03-05 14:14:26 +0100

    right place for the cronjob

--- docker/with-nginx/script.sh
+++ docker/with-nginx/script.sh
@@ -2,6 +2,6 @@
 
 sed -i 's/placeholder/'$NGINX_HOST'/' /etc/nginx/sites-enabled/fosscord.conf
 certbot --nginx --agree-tos -m $MAIL_CERTBOT --domains $NGINX_HOST --non-interactive --test-cert
-SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontab > /dev/null
+SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontabs/root > /dev/null
 nginx -s stop
 nginx -g "daemon off;"
