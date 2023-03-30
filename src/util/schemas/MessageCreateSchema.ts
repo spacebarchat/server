8b2faf0b18336e5dff1eeff4e849bcfd96b09e88
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-03-31 02:15:42 +1100

    SPACEBAR

--- src/util/schemas/MessageCreateSchema.ts
+++ src/util/schemas/MessageCreateSchema.ts
@@ -1,6 +1,6 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
-	Copyright (C) 2023 Fosscord and Fosscord Contributors
+	Copyright (C) 2023 Spacebar and Spacebar Contributors
 	
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
