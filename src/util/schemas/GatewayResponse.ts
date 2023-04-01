ed724472b364ee57288299e8e2bfc2ead1e633de
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2023-04-01 19:01:34 +1100

    gatewayresponse??

--- src/util/schemas/GatewayResponse.ts
+++ src/util/schemas/GatewayResponse.ts
@@ -1,6 +1,6 @@
 /*
-	Fosscord: A FOSS re-implementation and extension of the Discord.com backend.
-	Copyright (C) 2023 Fosscord and Fosscord Contributors
+	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
+	Copyright (C) 2023 Spacebar and Spacebar Contributors
 	
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
@@ -16,25 +16,6 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { Config } from "@fosscord/util";
-import { Router, Response, Request } from "express";
-import { route, RouteOptions } from "@fosscord/api";
-
-const router = Router();
-
-const options: RouteOptions = {
-	test: {
-		response: {
-			body: "GatewayResponse",
-		},
-	},
-};
-
-router.get("/", route(options), (req: Request, res: Response) => {
-	const { endpointPublic } = Config.get().gateway;
-	res.json({
-		url: endpointPublic || process.env.GATEWAY || "ws://localhost:3001",
-	});
-});
-
-export default router;
+export interface GatewayResponse {
+	url: string;
+}
