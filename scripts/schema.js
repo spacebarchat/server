776065c4978098e76b86662f5967d1597470d9ca
Stilic <lateman-jpeg@outlook.fr>
2023-04-16 08:02:50 +0200

    wip stuff

--- scripts/schema.js
+++ scripts/schema.js
@@ -22,122 +22,130 @@
 
 const path = require("path");
 const fs = require("fs");
-const TJS = require("typescript-json-schema");
+const tsj = require("ts-json-schema-generator");
 const walk = require("./util/walk");
-const schemaPath = path.join(__dirname, "..", "assets", "schemas.json");
 
-const settings = {
-	required: true,
-	ignoreErrors: true,
-	excludePrivate: true,
-	defaultNumberType: "integer",
-	noExtraProps: true,
-	defaultProps: false,
+// const settings = {
+// 	required: true,
+// 	ignoreErrors: true,
+// 	excludePrivate: true,
+// 	defaultNumberType: "integer",
+// 	noExtraProps: true,
+// 	defaultProps: false,
+// };
+// const excluded = [
+// 	"DefaultSchema",
+// 	"Schema",
+// 	"EntitySchema",
+// 	"ServerResponse",
+// 	"Http2ServerResponse",
+// 	"global.Express.Response",
+// 	"Response",
+// 	"e.Response",
+// 	"request.Response",
+// 	"supertest.Response",
+
+// 	// TODO: Figure out how to exclude schemas from node_modules?
+// 	"SomeJSONSchema",
+// 	"UncheckedPartialSchema",
+// 	"PartialSchema",
+// 	"UncheckedPropertiesSchema",
+// 	"PropertiesSchema",
+// 	"AsyncSchema",
+// 	"AnySchema",
+// 	"SMTPConnection.CustomAuthenticationResponse",
+// 	"TransportMakeRequestResponse",
+// ];
+
+/** @type {import('ts-json-schema-generator/dist/src/Config').Config} */
+const config = {
+	path: path.join(__dirname, "..", "src", "util", "schemas", "**", "*.ts"),
+	tsconfig: path.join(__dirname, "..", "tsconfig.json"),
+	skipTypeCheck: true,
+	additionalProperties: false,
+	strictTuples: true,
+	discriminatorType: "open-api",
+	minify: true,
+	type: "*", // TODO: set specific types after parsing on the program
 };
-const compilerOptions = {
-	strictNullChecks: true,
-};
-const Excluded = [
-	"DefaultSchema",
-	"Schema",
-	"EntitySchema",
-	"ServerResponse",
-	"Http2ServerResponse",
-	"global.Express.Response",
-	"Response",
-	"e.Response",
-	"request.Response",
-	"supertest.Response",
 
-	// TODO: Figure out how to exclude schemas from node_modules?
-	"SomeJSONSchema",
-	"UncheckedPartialSchema",
-	"PartialSchema",
-	"UncheckedPropertiesSchema",
-	"PropertiesSchema",
-	"AsyncSchema",
-	"AnySchema",
-	"SMTPConnection.CustomAuthenticationResponse",
-	"TransportMakeRequestResponse",
-];
+const program = createProgram(config);
+const generator = new SchemaGenerator(
+	program,
+	createParser(program, config, (prs) => {
+		prs.addNodeParser(new MyConstructorParser());
+	}),
+	createFormatter(config),
+	config,
+);
 
-function modify(obj) {
-	for (var k in obj) {
-		if (typeof obj[k] === "object" && obj[k] !== null) {
-			modify(obj[k]);
-		}
-	}
-}
+// fs.writeFile(path.join(__dirname, "..", "assets", "schemas.json"), JSON.stringify(generator.createSchema(config.type), null, 2), (err) => {
+//     if (err) throw err;
+// });
 
-function main() {
-	const program = TJS.programFromConfig(
-		path.join(__dirname, "..", "tsconfig.json"),
-		walk(path.join(__dirname, "..", "src", "util", "schemas")),
-	);
-	const generator = TJS.buildGenerator(program, settings);
-	if (!generator || !program) return;
+// const program = TJS.programFromConfig(
+// 	path.join(__dirname, "..", "tsconfig.json"),
+// 	walk(path.join(__dirname, "..", "src", "util", "schemas")),
+// );
+// const generator = TJS.buildGenerator(program, settings);
+// if (!generator || !program) return;
 
-	let schemas = generator.getUserSymbols().filter((x) => {
-		return (
-			(x.endsWith("Schema") ||
-				x.endsWith("Response") ||
-				x.startsWith("API")) &&
-			!Excluded.includes(x)
-		);
-	});
+// let schemas = generator.getUserSymbols().filter((x) => {
+// 	return (
+// 		(x.endsWith("Schema") ||
+// 			x.endsWith("Response") ||
+// 			x.startsWith("API")) &&
+// 		!Excluded.includes(x)
+// 	);
+// });
 
-	var definitions = {};
+// var definitions = {};
 
-	for (const name of schemas) {
-		const part = TJS.generateSchema(program, name, settings, [], generator);
-		if (!part) continue;
+// for (const name of schemas) {
+// 	const part = TJS.generateSchema(program, name, settings, [], generator);
+// 	if (!part) continue;
 
-		// this is a hack. want some want to check if its a @column, instead
-		if (part.properties) {
-			for (let key in part.properties) {
-				if (
-					[
-						// BaseClass methods
-						"toJSON",
-						"hasId",
-						"save",
-						"remove",
-						"softRemove",
-						"recover",
-						"reload",
-						"assign",
-					].includes(key)
-				) {
-					delete part.properties[key];
-					continue;
-				}
+// 	// this is a hack. want some want to check if its a @column, instead
+// 	if (part.properties) {
+// 		for (let key in part.properties) {
+// 			if (
+// 				[
+// 					// BaseClass methods
+// 					"toJSON",
+// 					"hasId",
+// 					"save",
+// 					"remove",
+// 					"softRemove",
+// 					"recover",
+// 					"reload",
+// 					"assign",
+// 				].includes(key)
+// 			) {
+// 				delete part.properties[key];
+// 				continue;
+// 			}
 
-				// if (part.properties[key].anyOf) {
-				// 	const nullIndex = part.properties[key].anyOf.findIndex(
-				// 		(x) => x.type == "null",
-				// 	);
-				// 	if (nullIndex != -1) {
-				// 		part.properties[key].nullable = true;
-				// 		part.properties[key].anyOf.splice(nullIndex, 1);
+// 			// if (part.properties[key].anyOf) {
+// 			// 	const nullIndex = part.properties[key].anyOf.findIndex(
+// 			// 		(x) => x.type == "null",
+// 			// 	);
+// 			// 	if (nullIndex != -1) {
+// 			// 		part.properties[key].nullable = true;
+// 			// 		part.properties[key].anyOf.splice(nullIndex, 1);
 
-				// 		if (part.properties[key].anyOf.length == 1) {
-				// 			Object.assign(
-				// 				part.properties[key],
-				// 				part.properties[key].anyOf[0],
-				// 			);
-				// 			delete part.properties[key].anyOf;
-				// 		}
-				// 	}
-				// }
-			}
-		}
+// 			// 		if (part.properties[key].anyOf.length == 1) {
+// 			// 			Object.assign(
+// 			// 				part.properties[key],
+// 			// 				part.properties[key].anyOf[0],
+// 			// 			);
+// 			// 			delete part.properties[key].anyOf;
+// 			// 		}
+// 			// 	}
+// 			// }
+// 		}
+// 	}
 
-		definitions = { ...definitions, [name]: { ...part } };
-	}
+// 	definitions = { ...definitions, [name]: { ...part } };
+// }
 
-	//modify(definitions);
-
-	fs.writeFileSync(schemaPath, JSON.stringify(definitions, null, 4));
-}
-
-main();
+// fs.writeFileSync(schemaPath, JSON.stringify(definitions, null, 4));
