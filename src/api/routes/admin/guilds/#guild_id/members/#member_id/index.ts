67c21d114d9b70d7316882c7746c96dfd616e1b0
Puyodead1 <puyodead@proton.me>
2023-12-17 17:30:32 -0500

    implement admin patch/delete guild members

--- src/api/routes/admin/guilds/#guild_id/members/#member_id/index.ts
+++ src/api/routes/admin/guilds/#guild_id/members/#member_id/index.ts
@@ -17,9 +17,89 @@
 */
 
 import { route } from "@spacebar/api";
-import { Member } from "@spacebar/util";
+import {
+	GuildMemberUpdateEvent,
+	Member,
+	MemberChangeSchema,
+	Role,
+	emitEvent,
+	handleFile,
+} from "@spacebar/util";
 import { Request, Response, Router } from "express";
 const router = Router();
+
+router.patch(
+	"/",
+	route({
+		description: "Update a guild member",
+		requestBody: "MemberChangeSchema",
+		right: "ADMIN_UPDATE_MEMBERS",
+		responses: {
+			200: {
+				body: "Member",
+			},
+			400: {
+				body: "APIErrorResponse",
+			},
+			403: {
+				body: "APIErrorResponse",
+			},
+			404: {
+				body: "APIErrorResponse",
+			},
+		},
+	}),
+	async (req: Request, res: Response) => {
+		const { guild_id, member_id } = req.params;
+		const body = req.body as MemberChangeSchema;
+
+		const member = await Member.findOneOrFail({
+			where: { id: member_id, guild_id },
+			relations: ["roles", "user"],
+		});
+
+		if ("nick" in body) {
+			if (!body.nick) {
+				delete body.nick;
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				//@ts-ignore shut up
+				member.nick = null; // remove the nickname
+			}
+		}
+
+		if (body.avatar)
+			body.avatar = await handleFile(
+				`/guilds/${guild_id}/users/${member_id}/avatars`,
+				body.avatar as string,
+			);
+
+		member.assign(body);
+
+		// must do this after the assign because the body roles array
+		// is string[] not Role[]
+		if ("roles" in body) {
+			body.roles = body.roles || [];
+			body.roles.filter((x) => !!x);
+
+			if (body.roles.indexOf(guild_id) === -1) body.roles.push(guild_id);
+			// foreign key constraint will fail if role doesn't exist
+			member.roles = body.roles.map((x) => Role.create({ id: x }));
+		}
+
+		await member.save();
+
+		member.roles = member.roles.filter((x) => x.id !== guild_id);
+
+		// do not use promise.all as we have to first write to db before emitting the event to catch errors
+		await emitEvent({
+			event: "GUILD_MEMBER_UPDATE",
+			guild_id,
+			data: { ...member, roles: member.roles.map((x) => x.id) },
+		} as GuildMemberUpdateEvent);
+
+		res.json(member);
+	},
+);
 
 router.delete(
 	"/",
