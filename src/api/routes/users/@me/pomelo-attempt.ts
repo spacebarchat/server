ca8b033f785ebfd2130c2e624ea29789800ab035
Puyodead1 <puyodead@protonmail.com>
2023-12-23 17:31:56 -0500

    implement authed pomelo routes

---
+++ src/api/routes/users/@me/pomelo-attempt.ts
@@ -0,0 +1,35 @@
+import { route } from "@spacebar/api";
+import { Config, UniqueUsernameAttemptSchema, User } from "@spacebar/util";
+import { Request, Response, Router } from "express";
+import { HTTPError } from "lambert-server";
+const router = Router();
+
+// https://discord-userdoccers.vercel.app/resources/user#get-pomelo-eligibility
+router.post(
+	"/",
+	route({
+		requestBody: "UniqueUsernameAttemptSchema",
+		responses: {
+			200: { body: "UniqueUsernameAttemptResponse" },
+			400: { body: "APIErrorResponse" },
+		},
+		description:
+			"Checks whether a unique username is available for the user to claim.",
+	}),
+	async (req: Request, res: Response) => {
+		const body = req.body as UniqueUsernameAttemptSchema;
+		const { uniqueUsernames } = Config.get().general;
+		if (!uniqueUsernames) {
+			throw new HTTPError(
+				"Unique Usernames feature is not enabled on this instance.",
+				400,
+			);
+		}
+
+		res.json({
+			taken: !(await User.isUsernameAvailable(body.username)),
+		});
+	},
+);
+
+export default router;
