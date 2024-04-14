ee59ee7e1cf4c5b9ae749745471a1f98053c3c58
Puyodead1 <puyodead@proton.me>
2024-04-14 14:02:22 -0400

    Fix message flags being null

---
+++ src/util/migration/postgres/1713116476900-messageFlagsNotNull.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MessageFlagsNotNull1713116476900 implements MigrationInterface {
+	name = "MessageFlagsNotNull1713116476900";
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			"ALTER TABLE messages RENAME COLUMN flags TO flags_old;",
+		);
+		await queryRunner.query(
+			"ALTER TABLE messages ADD COLUMN flags integer NOT NULL DEFAULT 0;",
+		);
+		await queryRunner.query(
+			"UPDATE messages SET flags = COALESCE(flags_old, 0);",
+		);
+		await queryRunner.query("ALTER TABLE messages DROP COLUMN flags_old;");
+	}
+
+	public async down(): Promise<void> {
+		// dont care
+		throw new Error("Migration down is not implemented.");
+	}
+}
