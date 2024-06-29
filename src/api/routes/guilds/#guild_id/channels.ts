e116e936936025a27ec75d915b680f981ce564e9
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2024-06-29 14:22:19 +0200

    Add <Channel>.position to public endpoints

--- src/api/routes/guilds/#guild_id/channels.ts
+++ src/api/routes/guilds/#guild_id/channels.ts
@@ -1,17 +1,17 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
 	Copyright (C) 2023 Spacebar and Spacebar Contributors
-	
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
 	by the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -40,6 +40,14 @@ router.get(
 	async (req: Request, res: Response) => {
 		const { guild_id } = req.params;
 		const channels = await Channel.find({ where: { guild_id } });
+
+		for await (const channel of channels) {
+			channel.position = await Channel.calculatePosition(
+				channel.id,
+				guild_id,
+				channel.guild,
+			);
+		}
 
 		res.json(channels);
 	},
@@ -70,6 +78,11 @@ router.post(
 		const channel = await Channel.createChannel(
 			{ ...body, guild_id },
 			req.user_id,
+		);
+		channel.position = await Channel.calculatePosition(
+			channel.id,
+			guild_id,
+			channel.guild,
 		);
 
 		res.status(201).json(channel);
