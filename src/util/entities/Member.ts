6d23cb0a39632558be7cd53edc9af6560be29df5
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2024-06-29 19:40:08 +0200

    Send position property on guild join

--- src/util/entities/Member.ts
+++ src/util/entities/Member.ts
@@ -1,17 +1,17 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
 	Copyright (C) 2023 Spacebar and Spacebar Contributors
-	
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
 	by the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -31,7 +31,7 @@ import {
 	PrimaryGeneratedColumn,
 	RelationId,
 } from "typeorm";
-import { Ban, PublicGuildRelations } from ".";
+import { Ban, Channel, PublicGuildRelations } from ".";
 import { ReadyGuildDTO } from "../dtos";
 import {
 	GuildCreateEvent,
@@ -329,6 +329,13 @@ export class Member extends BaseClassWithoutId {
 			relations: PublicGuildRelations,
 			relationLoadStrategy: "query",
 		});
+
+		for await (const channel of guild.channels) {
+			channel.position = await Channel.calculatePosition(
+				channel.id,
+				guild_id,
+			);
+		}
 
 		const memberCount = await Member.count({ where: { guild_id } });
 
