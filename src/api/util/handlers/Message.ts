7698a15f030732f4c855ec11a9c08538be037d78
Cyber <mateusz.hf18@gmail.com>
2024-07-09 17:19:28 +0200

    fix: check for message components

--- src/api/util/handlers/Message.ts
+++ src/api/util/handlers/Message.ts
@@ -140,7 +140,8 @@ export async function handleMessage(opts: MessageOptions): Promise<Message> {
 		!opts.embeds?.length &&
 		!opts.attachments?.length &&
 		!opts.sticker_ids?.length &&
-		!opts.poll
+		!opts.poll &&
+		!opts.components?.length
 	) {
 		throw new HTTPError("Empty messages are not allowed", 50006);
 	}
