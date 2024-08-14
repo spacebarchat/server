dcffccb6870cb3a7751902a0ea5680425f4c9d89
Puyodead1 <puyodead@proton.me>
2024-08-14 11:07:18 -0400

    Update 1723644478176-webhookSourceChannel.ts

--- src/util/migration/postgres/1723644478176-webhookSourceChannel.ts
+++ src/util/migration/postgres/1723644478176-webhookSourceChannel.ts
@@ -5,7 +5,7 @@ export class WebhookSourceChannel1723644478176 implements MigrationInterface {
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
-			"ALTER TABLE webhooks ADD COLUMN source_channel_id VARCHAR(255) NULL DEFAULT NULL AFTER source_guild_id",
+			"ALTER TABLE webhooks ADD COLUMN source_channel_id VARCHAR(255) NULL DEFAULT NULL",
 		);
 		await queryRunner.query(
 			"ALTER TABLE webhooks ADD CONSTRAINT FK_d64f38834fa676f6caa4786ddd6 FOREIGN KEY (source_channel_id) REFERENCES channels (id) ON UPDATE NO ACTION ON DELETE CASCADE",
