917f394cc562bbf5c23b8a14ee0c8d70783bc29d
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2024-08-18 12:05:50 +0200

    Consistent widget disabled error

--- src/api/routes/guilds/#guild_id/widget.png.ts
+++ src/api/routes/guilds/#guild_id/widget.png.ts
@@ -1,17 +1,17 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
 	Copyright (C) 2023 Spacebar and Spacebar Contributors
-	
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
 	by the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -19,7 +19,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { route } from "@spacebar/api";
-import { Guild } from "@spacebar/util";
+import { DiscordApiErrors, Guild } from "@spacebar/util";
 import { Request, Response, Router } from "express";
 import fs from "fs";
 import { HTTPError } from "lambert-server";
@@ -48,7 +48,7 @@ router.get(
 		const { guild_id } = req.params;
 
 		const guild = await Guild.findOneOrFail({ where: { id: guild_id } });
-		if (!guild.widget_enabled) throw new HTTPError("Unknown Guild", 404);
+		if (!guild.widget_enabled) throw DiscordApiErrors.EMBED_DISABLED;
 
 		// Fetch guild information
 		const icon = guild.icon;
