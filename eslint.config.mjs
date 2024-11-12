a11f3c04096f9019ce0cee11e853ea98d92afc45
CyberL1 <mateusz.hf18@gmail.com>
2024-11-12 13:48:52 +0100

    fix: return user profile theme colors as numbers

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "**/node_modules",
+        "**/dist",
+        "**/README.md",
+        "**/COPYING",
+        "src/webrtc",
+        "**/scripts/",
+        "**/assets",
+    ],
+}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+    },
+
+    rules: {
+        "no-mixed-spaces-and-tabs": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-var-requires": "off",
+    },
+}];
