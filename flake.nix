375e3fe189eca31bb90a4676ce8ac928956f38d2
Emma [it/its]@Rory& <root@rory.gay>
2024-12-25 20:52:37 +0100

    Update nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Spacebar server, written in Typescript.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
