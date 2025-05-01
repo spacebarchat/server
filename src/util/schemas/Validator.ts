58779f4b5dc49ae364a3d625b4a77375a0c331e7
MathMan05 <mathmanrm@gmail.com>
2025-04-30 19:11:43 -0500

    delete the null eater

--- src/util/schemas/Validator.ts
+++ src/util/schemas/Validator.ts
@@ -46,44 +46,7 @@ export const ajv = new Ajv({
 addFormats(ajv);
 
 export function validateSchema<G extends object>(schema: string, data: G): G {
-	const valid = ajv.validate(schema, normalizeBody(data));
+	const valid = ajv.validate(schema, data);
 	if (!valid) throw ajv.errors;
 	return data;
 }
-
-// Normalizer is introduced to workaround https://github.com/ajv-validator/ajv/issues/1287
-// this removes null values as ajv doesn't treat them as undefined
-// normalizeBody allows to handle circular structures without issues
-// taken from https://github.com/serverless/serverless/blob/master/lib/classes/ConfigSchemaHandler/index.js#L30 (MIT license)
-export const normalizeBody = (body: object = {}) => {
-	const normalizedObjectsSet = new WeakSet();
-	const normalizeObject = (object: object) => {
-		if (normalizedObjectsSet.has(object)) return;
-		normalizedObjectsSet.add(object);
-		if (Array.isArray(object)) {
-			for (const [, value] of object.entries()) {
-				if (typeof value === "object") normalizeObject(value);
-			}
-		} else {
-			for (const [key, value] of Object.entries(object)) {
-				if (value == null) {
-					if (
-						key === "icon" ||
-						key === "avatar" ||
-						key === "banner" ||
-						key === "splash" ||
-						key === "discovery_splash"
-					)
-						continue;
-					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					//@ts-ignore
-					delete object[key];
-				} else if (typeof value === "object") {
-					normalizeObject(value);
-				}
-			}
-		}
-	};
-	normalizeObject(body);
-	return body;
-};
