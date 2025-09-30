8cfe0311ca9451a162f7a9d7fb076d07a48ccf2f
Rory& <root@rory.gay>
2025-09-30 05:11:08 +0200

    Copyright notices on email handling

--- src/util/util/email/index.ts
+++ src/util/util/email/index.ts
@@ -1,6 +1,6 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
-	Copyright (C) 2023 Spacebar and Spacebar Contributors
+	Copyright (C) 2025 Spacebar and Spacebar Contributors
 	
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
