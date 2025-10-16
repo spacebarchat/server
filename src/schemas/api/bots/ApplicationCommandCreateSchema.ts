df1a4ca6f7f70af616982017e659bd2bd0850c1e
CyberL1 <cyber.hf18@gmail.com>
2025-10-16 22:28:32 +0200

    chore: add `ApplicationCommandCreateSchema`

---
+++ src/schemas/api/bots/ApplicationCommandCreateSchema.ts
@@ -0,0 +1,19 @@
+import { ApplicationCommandHandlerType, ApplicationCommandOption, ApplicationCommandType, ApplicationIntegrationType, InteractionContextType } from "@spacebar/schemas";
+
+export interface ApplicationCommandCreateSchema {
+	type?: ApplicationCommandType;
+	name: string;
+	name_localizations?: Record<string, string>;
+	description?: string;
+	description_localizations?: Record<string, string>;
+	options?: ApplicationCommandOption[];
+	default_member_permissions?: string;
+	/*
+	 * @deprecated
+	 */
+	dm_permission?: boolean;
+	nsfw?: boolean;
+	integration_types?: ApplicationIntegrationType[];
+	contexts?: InteractionContextType[];
+	handler?: ApplicationCommandHandlerType;
+}
--- src/schemas/api/bots/index.ts
+++ src/schemas/api/bots/index.ts
@@ -15,4 +15,5 @@
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+export * from "./ApplicationCommandCreateSchema";
 export * from "./ApplicationCommandSchema";
