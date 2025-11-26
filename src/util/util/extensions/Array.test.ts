aa40eee0975782771994f83ed14cfc9ee9e4c3ab
MathMan05 <mathmanrm@gmail.com>
2025-11-26 14:48:15 -0600

    remove test, oops

--- src/util/util/extensions/Array.test.ts
+++ src/util/util/extensions/Array.test.ts
@@ -5,13 +5,6 @@ import { describe, it } from "node:test";
 import assert from "node:assert/strict";
 
 describe("Array extensions", () => {
-	it("partition", () => {
-		const arr = [1, 2, 3, 4, 5];
-		const [even, odd] = arr.partition((n) => n % 2 === 0);
-		assert.deepEqual(even, [2, 4]);
-		assert.deepEqual(odd, [1, 3, 5]);
-	});
-
 	it("remove", () => {
 		const arr = [1, 2, 3, 4, 5];
 		arr.remove(3);
