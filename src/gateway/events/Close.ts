fcb24b1bac67fdcf901a4195f177f339ea50bdcc
Rory& <root@rory.gay>
2025-11-29 22:35:03 +0100

    Handle case where session is an object, merges #1412

--- src/gateway/events/Close.ts
+++ src/gateway/events/Close.ts
@@ -91,7 +91,7 @@ export async function Close(this: WebSocket, code: number, reason: Buffer) {
 				user: userOrId,
 				activities: session.activities,
 				client_status: session?.client_status,
-				status: session.getPublicStatus(),
+				status: session.getPublicStatus?.() ?? session.status,
 			},
 		} as PresenceUpdateEvent);
 	}
