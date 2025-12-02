bd93ba0e991fb30f5fae9cfd705a3015381634df
Rory& <root@rory.gay>
2025-12-02 07:17:30 +0100

    Template billing routes

--- src/api/routes/users/@me/billing/payment-sources.ts
+++ src/api/routes/users/@me/billing/payment-sources.ts
@@ -21,9 +21,61 @@ import { route } from "@spacebar/api";
 
 const router = Router({ mergeParams: true });
 
+// https://docs.discord.food/resources/billing#example-payment-source
+const example = {
+	id: "1422548914485198869",
+	type: 1,
+	invalid: false,
+	flags: 2,
+	deleted_at: null,
+	brand: "visa",
+	last_4: "4242",
+	expires_month: 9,
+	expires_year: 2077,
+	billing_address: {
+		name: "John Doe",
+		line_1: "123 Main Street",
+		line_2: "Apt 4B",
+		city: "San Francisco",
+		state: "CA",
+		country: "US",
+		postal_code: "94105",
+	},
+	country: "US",
+	payment_gateway: 1,
+	payment_gateway_source_id: "pm_DwiVlGlYwe1qxLzy4QWChQeo",
+	default: false,
+};
+
 router.get("/", route({}), (req: Request, res: Response) => {
-	// TODO:
-	res.json([]).status(200);
+	// TODO: schema
+	res.json([example]).status(200);
+});
+
+router.post("/", route({}), (req: Request, res: Response) => {
+	// TODO: schema
+	res.json([example]).status(200);
+});
+
+router.get("/:payment_source_id", route({}), (req: Request, res: Response) => {
+	// TODO: schema
+	res.json({
+		...example,
+		id: req.route.payment_source_id,
+	}).status(200);
+});
+
+router.patch("/:payment_source_id", route({}), (req: Request, res: Response) => {
+	// TODO: schema
+	res.json({
+		...example,
+		id: req.route.payment_source_id,
+	}).status(200);
+});
+
+router.delete("/:payment_source_id", route({}), (req: Request, res: Response) => {
+	// TODO: schema
+	res.status(204);
 });
 
 export default router;
