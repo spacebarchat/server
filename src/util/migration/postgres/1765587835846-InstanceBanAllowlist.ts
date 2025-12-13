3f5e25dd3fe94e17dd01c9d3c93fe9143a4ba194
Rory& <root@rory.gay>
2025-12-13 02:04:24 +0100

    Add missing migration

---
+++ src/util/migration/postgres/1765587835846-InstanceBanAllowlist.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InstanceBanAllowlist1765587835846 implements MigrationInterface {
+    name = 'InstanceBanAllowlist1765587835846'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "instance_bans" ADD "is_allowlisted" boolean NOT NULL DEFAULT false`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "instance_bans" DROP COLUMN "is_allowlisted"`);
+    }
+
+}
