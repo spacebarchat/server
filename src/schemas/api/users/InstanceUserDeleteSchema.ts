eafe79095e6915b81d0e0f2d7735f561cd6f9129
Rory& <root@rory.gay>
2025-12-16 05:00:49 +0100

    Update instance ban schema

--- src/schemas/api/users/InstanceUserDeleteSchema.ts
+++ src/schemas/api/users/InstanceUserDeleteSchema.ts
@@ -3,4 +3,5 @@ import { ConnectedAccount } from "@spacebar/util";
 export type InstanceUserDeleteSchema = InstanceUserDeleteSchemaContent | undefined; //unsure if this a correct way to make the body optional
 export interface InstanceUserDeleteSchemaContent {
 	reason?: string;
+	persistInstanceBan?: boolean;
 }
