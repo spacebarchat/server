46b9094f3d70f76d1254a9b438d2e41a7f3b4ac5
Rory& <root@rory.gay>
2025-12-21 02:02:00 +0100

    Filter widget channels by type, implement rudimentary cache, mark cache as immutable

--- src/api/routes/guilds/#guild_id/widget.json.ts
+++ src/api/routes/guilds/#guild_id/widget.json.ts
@@ -19,6 +19,7 @@
 import { randomString, route } from "@spacebar/api";
 import { Channel, Config, DiscordApiErrors, Guild, Invite, Member, Permissions } from "@spacebar/util";
 import { Request, Response, Router } from "express";
+import { ChannelType, GuildWidgetJsonResponse } from "@spacebar/schemas*";
 
 const router: Router = Router({ mergeParams: true });
 
@@ -31,6 +32,10 @@ const router: Router = Router({ mergeParams: true });
 
 // https://discord.com/developers/docs/resources/guild#get-guild-widget
 // TODO: Cache the response for a guild for 5 minutes regardless of response
+
+const expiryTime = 1000 * 60 * 5; // 5 minutes
+const jsonDataCache = new Map<string, { data: Promise<GuildWidgetJsonResponse>; expiry: Date }>();
+
 router.get(
     "/",
     route({
@@ -46,91 +51,107 @@ router.get(
     async (req: Request, res: Response) => {
         const { guild_id } = req.params;
 
-        const guild = await Guild.findOneOrFail({
-            where: { id: guild_id },
-            select: {
-                channel_ordering: true,
-                widget_channel_id: true,
-                widget_enabled: true,
-                presence_count: true,
-                name: true,
-            },
-        });
-        if (!guild.widget_enabled) throw DiscordApiErrors.EMBED_DISABLED;
-
-        // Fetch existing widget invite for widget channel
-        let invite = await Invite.findOne({
-            where: { channel_id: guild.widget_channel_id },
-        });
-
-        if (guild.widget_channel_id && !invite) {
-            // Create invite for channel if none exists
-            // TODO: Refactor invite create code to a shared function
-            const max_age = 86400; // 24 hours
-            const expires_at = new Date(max_age * 1000 + Date.now());
-
-            invite = await Invite.create({
-                code: randomString(),
-                temporary: false,
-                uses: 0,
-                max_uses: 0,
-                max_age: max_age,
-                expires_at,
-                created_at: new Date(),
-                guild_id,
-                channel_id: guild.widget_channel_id,
-                flags: 0,
-            }).save();
+        let cacheEntry = jsonDataCache.get(guild_id);
+        if (!cacheEntry || cacheEntry.expiry.getTime() < Date.now()) {
+            // Create new cache entry
+            const dataPromise = getWidgetJsonData(guild_id);
+            cacheEntry = {
+                data: dataPromise,
+                expiry: new Date(Date.now() + expiryTime),
+            };
+            console.log("[Widget] Caching widget data for guild", guild_id);
+            jsonDataCache.set(guild_id, cacheEntry);
         }
 
-        // Fetch voice channels, and the @everyone permissions object
-        const channels: { id: string; name: string; position: number }[] = [];
-
-        (await Channel.getOrderedChannels(guild.id, guild)).filter((doc) => {
-            // Only return channels where @everyone has the CONNECT permission
-            if (doc.permission_overwrites === undefined || Permissions.channelPermission(doc.permission_overwrites, Permissions.FLAGS.CONNECT) === Permissions.FLAGS.CONNECT) {
-                channels.push({
-                    id: doc.id,
-                    name: doc.name ?? "Unknown channel",
-                    position: doc.position ?? 0,
-                });
-            }
-        });
-
-        // Fetch members
-        // TODO: Understand how Discord's max 100 random member sample works, and apply to here (see top of this file)
-        const members = await Member.find({ where: { guild_id: guild_id }, relations: { user: { sessions: true } } });
-        const minLastSeen = Date.now() - 1000 * 60 * 5;
-        const onlineMembers = members.filter((m) => m.user.sessions.filter((s) => (s.last_seen?.getTime() ?? 0) > minLastSeen).length > 0);
-        const memberData = onlineMembers.map((x) => {
-            return {
-                id: x.id,
-                username: x.user.username,
-                discriminator: x.user.discriminator,
-                avatar: null,
-                status: "online", // TODO
-                avatar_url: x.avatar
-                    ? `${Config.get().cdn.endpointPublic}/guilds/${guild_id}/users/${x.id}/avatars/${x.avatar}.png`
-                    : x.user.avatar
-                      ? `${Config.get().cdn.endpointPublic}/avatars/${x.id}/${x.user.avatar}.png`
-                      : `${Config.get().cdn.endpointPublic}/embed/avatars/${BigInt(x.id) % 6n}.png`,
-            };
-        });
-
-        // Construct object to respond with
-        const data = {
-            id: guild_id,
-            name: guild.name,
-            instant_invite: invite?.code,
-            channels: channels,
-            members: memberData,
-            member_count: members.length,
-            presence_count: guild.presence_count || onlineMembers.length,
-        };
-
-        res.set("Cache-Control", "public, max-age=300");
-        return res.json(data);
+        const cacheRemainingSeconds = Math.floor((cacheEntry.expiry.getTime() - Date.now()) / 1000);
+        res.set("Cache-Control", `public, max-age=${cacheRemainingSeconds}, s-maxage=${cacheRemainingSeconds}, immutable`);
+        return res.json(await cacheEntry.data);
     },
 );
+
+async function getWidgetJsonData(guild_id: string) {
+    const guild = await Guild.findOneOrFail({
+        where: { id: guild_id },
+        select: {
+            channel_ordering: true,
+            widget_channel_id: true,
+            widget_enabled: true,
+            presence_count: true,
+            name: true,
+        },
+    });
+    if (!guild.widget_enabled) throw DiscordApiErrors.EMBED_DISABLED;
+
+    // Fetch existing widget invite for widget channel
+    let invite = await Invite.findOne({
+        where: { channel_id: guild.widget_channel_id },
+    });
+
+    if (guild.widget_channel_id && !invite) {
+        // Create invite for channel if none exists
+        // TODO: Refactor invite create code to a shared function
+        const max_age = 86400; // 24 hours
+        const expires_at = new Date(max_age * 1000 + Date.now());
+
+        invite = await Invite.create({
+            code: randomString(),
+            temporary: false,
+            uses: 0,
+            max_uses: 0,
+            max_age: max_age,
+            expires_at,
+            created_at: new Date(),
+            guild_id,
+            channel_id: guild.widget_channel_id,
+            flags: 0,
+        }).save();
+    }
+
+    // Fetch voice channels, and the @everyone permissions object
+    const channels: { id: string; name: string; position: number }[] = [];
+
+    (await Channel.getOrderedChannels(guild.id, guild)).filter((doc) => {
+        if (doc.type !== ChannelType.GUILD_VOICE) return false;
+        // Only return voice channels where @everyone has the CONNECT permission
+        if (doc.permission_overwrites === undefined || Permissions.channelPermission(doc.permission_overwrites, Permissions.FLAGS.CONNECT) === Permissions.FLAGS.CONNECT) {
+            channels.push({
+                id: doc.id,
+                name: doc.name ?? "Unknown channel",
+                position: doc.position ?? 0,
+            });
+        }
+    });
+
+    // Fetch members
+    // TODO: Understand how Discord's max 100 random member sample works, and apply to here (see top of this file)
+    const members = await Member.find({ where: { guild_id: guild_id }, relations: { user: { sessions: true } } });
+    const minLastSeen = Date.now() - 1000 * 60 * 5;
+    const onlineMembers = members.filter((m) => m.user.sessions.filter((s) => (s.last_seen?.getTime() ?? 0) > minLastSeen).length > 0);
+    const memberData = onlineMembers.map((x) => {
+        return {
+            id: x.id,
+            username: x.user.username,
+            discriminator: x.user.discriminator,
+            avatar: null,
+            status: "online", // TODO
+            avatar_url: x.avatar
+                ? `${Config.get().cdn.endpointPublic}/guilds/${guild_id}/users/${x.id}/avatars/${x.avatar}.png`
+                : x.user.avatar
+                  ? `${Config.get().cdn.endpointPublic}/avatars/${x.id}/${x.user.avatar}.png`
+                  : `${Config.get().cdn.endpointPublic}/embed/avatars/${BigInt(x.id) % 6n}.png`,
+        };
+    });
+
+    // Construct object to respond with
+    return {
+        id: guild_id,
+        name: guild.name,
+        instant_invite: invite?.code,
+        channels: channels,
+        members: memberData,
+        member_count: members.length,
+        presence_count: guild.presence_count || onlineMembers.length,
+    } as GuildWidgetJsonResponse;
+}
 
 export default router;
