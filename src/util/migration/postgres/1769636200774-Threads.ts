1f1564539ee14a1903a7ba6ae499bbd293b5265c
Rory& <root@rory.gay>
2026-01-28 22:42:29 +0100

    Regenerate migrations for threads

--- src/util/migration/postgres/1769636200774-Threads.ts
+++ src/util/migration/postgres/1769636200774-Threads.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Threads1764612754204 implements MigrationInterface {
-    name = "Threads1764612754204";
+export class Threads1769636200774 implements MigrationInterface {
+    name = "Threads1769636200774";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "messages" ADD "thread_id" character varying`);
@@ -9,9 +9,13 @@ export class Threads1764612754204 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "channels" ADD "member_count" integer`);
         await queryRunner.query(`ALTER TABLE "channels" ADD "message_count" integer`);
         await queryRunner.query(`ALTER TABLE "channels" ADD "total_message_sent" integer`);
+        await queryRunner.query(
+            `ALTER TABLE "messages" ADD CONSTRAINT "FK_bb3af7f695d50083e6523290d41" FOREIGN KEY ("thread_id") REFERENCES "channels"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "messages" DROP CONSTRAINT "FK_bb3af7f695d50083e6523290d41"`);
         await queryRunner.query(`ALTER TABLE "channels" DROP COLUMN "total_message_sent"`);
         await queryRunner.query(`ALTER TABLE "channels" DROP COLUMN "message_count"`);
         await queryRunner.query(`ALTER TABLE "channels" DROP COLUMN "member_count"`);
