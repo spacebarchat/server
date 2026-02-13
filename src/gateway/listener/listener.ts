7d16d463e7f0d8422ec6756f2c37ba1d482e61e4
MathMan05 <mathmanrm@gmail.com>
2026-02-13 11:07:00 -0600

    fix guild join

--- src/gateway/listener/listener.ts
+++ src/gateway/listener/listener.ts
@@ -1,17 +1,17 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
 	Copyright (C) 2023 Spacebar and Spacebar Contributors
-	
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
 	by the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -245,7 +245,12 @@ async function consume(this: WebSocket, opts: EventOpts) {
             this.events[data.user.id] = await listenEvent(data.user.id, handlePresenceUpdate.bind(this), this.listen_options);
             break;
         case "GUILD_CREATE":
-            this.events[id] = await listenEvent(id, consumer, listenOpts);
+            Promise.all([
+                ...data.channels.map(async ({ id }: { id: string }) => {
+                    this.events[id] = await listenEvent(id, consumer, listenOpts);
+                }),
+                listenEvent(id, consumer, listenOpts).then((ret) => (this.events[id] = ret)),
+            ]);
             break;
         case "CHANNEL_UPDATE": {
             const exists = this.events[id];
