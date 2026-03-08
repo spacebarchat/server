f4e303f5a517f8eed4daeeb1fc99bc33dde4516d
Rory& <root@rory.gay>
2026-03-08 16:24:54 +0100

    Migrate InteractionCreateEvent to satisfies, part of #1577

--- src/api/routes/interactions/index.ts
+++ src/api/routes/interactions/index.ts
@@ -39,7 +39,7 @@ router.post("/", route({}), async (req: Request, res: Response) => {
             id: interactionId,
             nonce: body.nonce,
         },
-    } as InteractionCreateEvent);
+    } satisfies InteractionCreateEvent);
 
     const user = req.user;
 
@@ -97,8 +97,12 @@ router.post("/", route({}), async (req: Request, res: Response) => {
     emitEvent({
         event: "INTERACTION_CREATE",
         user_id: body.application_id,
-        data: interactionData,
-    } as InteractionCreateEvent);
+        data: {
+            ...interactionData,
+            member_id: req.user_id, // TODO: is this correct?
+            id: interactionId, // TODO: is this correct?
+        },
+    } satisfies InteractionCreateEvent);
 
     const interactionTimeout = setTimeout(() => {
         emitEvent({
