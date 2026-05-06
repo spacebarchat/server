8ef266ca25f522e798fa5f914fbfe2f6b9cd414b
samuelscheit <github@samuelscheit.com>
2026-05-06 02:54:30 +0200

    Move guild welcome screen DTO to schemas

--- src/util/interfaces/GuildWelcomeScreen.ts
+++ src/util/interfaces/GuildWelcomeScreen.ts
@@ -1,10 +1,1 @@
-export interface GuildWelcomeScreen {
-    enabled: boolean;
-    description: string;
-    welcome_channels: {
-        description: string;
-        emoji_id?: string;
-        emoji_name?: string;
-        channel_id: string;
-    }[];
-}
+export type { GuildWelcomeScreen } from "@spacebar/schemas";
