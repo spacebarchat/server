05db5134bbb53dbb5b5ae02f556c76252e7b3eae
Rory& <root@rory.gay>
2026-07-21 23:58:00 +0200

    TraverseDirectory: allow specifying a destination router

--- src/util/util/TraverseDirectory.ts
+++ src/util/util/TraverseDirectory.ts
@@ -17,12 +17,13 @@
 */
 
 import { Server, traverseDirectory } from "lambert-server";
+import { Router } from "express";
 
 //if we're using ts-node, use ts files instead of js
 const extension = Symbol.for("ts-node.register.instance") in process ? "ts" : "js";
 
 const DEFAULT_FILTER = new RegExp("^([^.].*)(?<!\\.d).(" + extension + ")$");
 
-export function registerRoutes(server: Server, root: string) {
-    return traverseDirectory({ dirname: root, recursive: true, filter: DEFAULT_FILTER }, server.registerRoute.bind(server, root));
+export function registerRoutes(server: Server, root: string, destRouter: Router | undefined = undefined) {
+    return traverseDirectory({ dirname: root, recursive: true, filter: DEFAULT_FILTER }, (file) => server.registerRoute(root, file, destRouter));
 }
