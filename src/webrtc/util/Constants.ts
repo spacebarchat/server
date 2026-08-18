964053bcb10fc7cc47dd9a7ded5994ec51b27f01
Rory& <root@rory.gay>
2026-08-18 19:51:47 +0200

    Voice opcodes

--- src/webrtc/util/Constants.ts
+++ src/webrtc/util/Constants.ts
@@ -29,12 +29,30 @@ export enum VoiceOPCodes {
     RESUME = 7,
     HELLO = 8,
     RESUMED = 9,
+    // 10 is unknown
+    CLIENTS_CONNECT = 11,
     VIDEO = 12,
     CLIENT_DISCONNECT = 13,
     SESSION_UPDATE = 14,
     MEDIA_SINK_WANTS = 15,
     VOICE_BACKEND_VERSION = 16,
     CHANNEL_OPTIONS_UPDATE = 17,
+    CLIENT_FLAGS = 18,
+    CLIENT_PLATFORM = 20,
+    // crypto crap...
+    DAVE_PROTOCOL_PREPARE_TRANSITION = 21,
+    DAVE_PROTOCOL_EXECUTE_TRANSITION = 22,
+    DAVE_PROTOCOL_TRANSITION_READY = 23,
+    DAVE_PROTOCOL_PREPARE_EPOCH = 24,
+    MLS_EXTERNAL_SENDER_PACKAGE = 25,
+    MLS_KEY_PACKAGE = 26,
+    MLS_PROPOSALS = 27,
+    MLS_COMMIT_WELCOME = 28,
+    MLS_ANNOUNCE_COMMIT_TRANSITION = 29,
+    MLS_WELCOME = 30,
+    MLS_INVALID_COMMIT_WELCOME = 31,
+    // no more cypto crap?
+    NO_ROUTE = 32,
 }
 
 export type VoicePayload = Omit<Payload, "op"> & { op: VoiceOPCodes };
