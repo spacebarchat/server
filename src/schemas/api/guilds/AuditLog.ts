3bfd76dc5a8004e4a10b055d1be81afc64707ad2
Rory& <root@rory.gay>
2026-08-31 04:52:22 +0200

    audit log event enum

--- src/schemas/api/guilds/AuditLog.ts
+++ src/schemas/api/guilds/AuditLog.ts
@@ -18,6 +18,7 @@
 
 import { AutomodRuleSchema, ChannelPermissionOverwrite, PartialUser, PublicChannel, WebhookResponse } from "@spacebar/schemas";
 
+// Note: some audit log event types have some special keys: https://docs.discord.food/resources/audit-log#audit-log-change-exceptions
 export interface AuditLogResponse {
     audit_log_entries: unknown[]; // TODO: type
     application_commands: unknown[]; // TODO: type
@@ -32,13 +33,6 @@ export interface AuditLogResponse {
 export enum AuditLogEvents {
     // guild level
     GUILD_UPDATE = 1,
-    GUILD_IMPORT = 2,
-    GUILD_EXPORTED = 3,
-    GUILD_ARCHIVE = 4,
-    GUILD_UNARCHIVE = 5,
-    // join-leave
-    USER_JOIN = 6,
-    USER_LEAVE = 7,
     // channels
     CHANNEL_CREATE = 10,
     CHANNEL_UPDATE = 11,
@@ -62,7 +56,6 @@ export enum AuditLogEvents {
     ROLE_CREATE = 30,
     ROLE_UPDATE = 31,
     ROLE_DELETE = 32,
-    ROLE_SWAP = 33,
     // invites
     INVITE_CREATE = 40,
     INVITE_UPDATE = 41,
@@ -71,15 +64,11 @@ export enum AuditLogEvents {
     WEBHOOK_CREATE = 50,
     WEBHOOK_UPDATE = 51,
     WEBHOOK_DELETE = 52,
-    WEBHOOK_SWAP = 53,
     // custom emojis
     EMOJI_CREATE = 60,
     EMOJI_UPDATE = 61,
     EMOJI_DELETE = 62,
-    EMOJI_SWAP = 63,
     // deletion
-    MESSAGE_CREATE = 70, // messages sent using non-primary seat of the user only
-    MESSAGE_EDIT = 71, // non-self edits only
     MESSAGE_DELETE = 72,
     MESSAGE_BULK_DELETE = 73,
     // pinning
@@ -97,26 +86,60 @@ export enum AuditLogEvents {
     STICKER_CREATE = 90,
     STICKER_UPDATE = 91,
     STICKER_DELETE = 92,
-    STICKER_SWAP = 93,
+    // scheduled events
+    GUILD_SCHEDULED_EVENT_CREATE = 100,
+    GUILD_SCHEDULED_EVENT_UPDATE = 101,
+    GUILD_SCHEDULED_EVENT_DELETE = 102,
     // threads
     THREAD_CREATE = 110,
     THREAD_UPDATE = 111,
     THREAD_DELETE = 112,
     // application commands
     APPLICATION_COMMAND_PERMISSION_UPDATE = 121,
+    // soundboard
+    SOUNDBOARD_SOUND_CREATE = 130,
+    SOUNDBOARD_SOUND_UPDATE = 131,
+    SOUNDBOARD_SOUND_DELETE = 132,
     // automod
-    POLICY_CREATE = 140,
-    POLICY_UPDATE = 141,
-    POLICY_DELETE = 142,
-    MESSAGE_BLOCKED_BY_POLICIES = 143, // in spacebar, blocked messages are stealth-dropped
-    // instance policies affecting the guild
-    GUILD_AFFECTED_BY_POLICIES = 216,
-    // message moves
-    IN_GUILD_MESSAGE_MOVE = 223,
-    CROSS_GUILD_MESSAGE_MOVE = 224,
-    // message routing
-    ROUTE_CREATE = 225,
-    ROUTE_UPDATE = 226,
+    AUTO_MODERATION_RULE_CREATE = 140,
+    AUTO_MODERATION_RULE_UPDATE = 141,
+    AUTO_MODERATION_RULE_DELETE = 142,
+    AUTO_MODERATION_BLOCK_MESSAGE = 143,
+    AUTO_MODERATION_FLAG_TO_CHANNEL = 144,
+    AUTO_MODERATION_USER_COMMUNICATION_DISABLE = 145,
+    AUTO_MODERATION_QUARANTINE_USER = 146,
+    // creator monetization
+    CREATOR_MONETIZATION_REQUEST_CREATED = 150,
+    CREATOR_MONETIZATION_TERMS_ACCEPTED = 151,
+    // onboarding
+    ONBOARDING_PROMPT_CREATE = 163,
+    ONBOARDING_PROMPT_UPDATE = 164,
+    ONBOARDING_PROMPT_DELETE = 165,
+    ONBOARDING_CREATE = 166,
+    ONBOARDING_UPDATE = 167,
+    // guild home
+    GUILD_HOME_FEATURE_ITEM = 171,
+    GUILD_HOME_REMOVE_ITEM = 172,
+    // ??
+    HARMFUL_LINKS_BLOCKED_MESSAGE = 180, // deprecated?
+    // new member welcome
+    HOME_SETTINGS_CREATE = 190,
+    HOME_SETTINGS_UPDATE = 191,
+    // voice channel status
+    VOICE_CHANNEL_STATUS_CREATE = 192,
+    VOICE_CHANNEL_STATUS_DELETE = 193,
+    // ??
+    CLYDE_AI_PROFILE_UPDATE = 194, // deprecated
+    // guild scheduled event exceptions
+    GUILD_SCHEDULED_EVENT_EXCEPTION_CREATE = 200,
+    GUILD_SCHEDULED_EVENT_EXCEPTION_UPDATE = 201,
+    GUILD_SCHEDULED_EVENT_EXCEPTION_DELETE = 202,
+    // member verification
+    GUILD_MEMBER_VERIFICATION_UPDATE = 210,
+    // ??
+    GUILD_PROFILE_UPDATE = 211,
+    GUILD_MIGRATE_PIN_PERMISSION = 212,
+    GUILD_MIGRATE_BYPASS_SLOWMODE_PERMISSION = 213,
 }
 
 export interface AuditLogChange {
